aed8dc9e4de88f2ed77f13466c023e127b2ef40a
marlonkeating <marlonakeating@gmail.com>
2017-07-15 16:11:13 -0700

    Add STATIC_ROOT configuration

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: gunicorn portal.wsgi
-release: python manage.py migrate --noinput
+release: python manage.py migrate --noinput && python manage.py collectstatic
--- democracylab/settings.py
+++ democracylab/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'democracylab.wsgi.application'
 DATABASES = {
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'NAME': os.path.join(PROJECT_ROOT, 'db.sqlite3'),
     # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
