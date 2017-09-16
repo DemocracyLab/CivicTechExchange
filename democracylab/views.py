afbd6ed61cfe9c1c2f5b455b0ab06831211888e5
Dino Viehland <dinoviehland@gmail.com>
2017-09-16 15:46:10 -0700

    Add extra fields to signup page

--- democracylab/views.py
+++ democracylab/views.py
@@ -5,6 +5,66 @@ from django.template import loader
 from django.http import HttpResponse
 
 from .forms import DemocracyLabUserCreationForm
+#from common.models.tags import Tag
+
+class Tag:
+    pass
+
+def tag(name):
+    res = Tag()
+    res.tag_name = name.lower().replace(' ', '_')
+    res.display_name = name
+    return res
+
+def tags(*tags):
+    return [tag(t) for t in tags]
+PROJECT_KINDS = tags(
+    '1st Amendment', 
+    '2nd Amendment', 
+    'Cultural Issues', 
+    'Economy',
+    'Education',
+    'Environment',
+    'Health Care',
+    'Homelessness',
+    'Housing',
+    'Immigration',
+    'International Issues',
+    'National Security',
+    'Political Reform',
+    'Public Safety',
+    'Social Justice',
+    'Taxes',
+    'Transportation',
+    'Other'
+)
+
+SKILL_KINDS = tags(
+    'Accounting',
+    'Back-End Development',
+    'Business Strategy',
+    'Copywriting',
+    'Data Science',
+    'Data Visualization',
+    'Database Architecture',
+    'Front-End Development',
+    'Fundraising',
+    'Graphic Design',
+    'Legal Services',
+    'Project Management',
+    'Research',
+    'UI Design',
+    'UX Design',
+    'Other'
+)
+
+def to_columns(items):
+    res = [[], [], []]
+    cur = 0
+    for item in items:
+        res[cur % 3].append(item)
+        cur+=1
+    return res
 
 def signup(request):
     if request.method == 'POST':
@@ -20,6 +80,9 @@ def signup(request):
         form = UserCreationForm()
 
     template = loader.get_template('signup.html')
-    context = {'form': form}
+    context = {'form': form,
+        'skills':to_columns(SKILL_KINDS),
+        'projects':to_columns(PROJECT_KINDS)
+    }
     return HttpResponse(template.render(context, request))
     # return render(request, 'signup.html', {'form': form})
