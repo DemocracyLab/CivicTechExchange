c543cd6f3e4efe89288aa774117df89b8a4c6613
Marlon Keating <marlonakeating@gmail.com>
2017-09-25 11:16:41 -0700

    Import tags from .csv during migration

--- common/models/tags.py
+++ common/models/tags.py
@@ -1,10 +1,34 @@
 from django.db import models
+import sys
+import os
+import csv
 
 # Create your models here.
 class Tag(models.Model):
-    tag_name = models.CharField(max_length=100)
+    tag_name = models.CharField(max_length=100, unique=True)
     display_name = models.CharField(max_length=200)
-    caption = models.CharField(max_length=500)
+    caption = models.CharField(max_length=500, blank=True)
     category = models.CharField(max_length=200)
-    subcategory = models.CharField(max_length=200)
-    parent = models.CharField(max_length=100)
+    subcategory = models.CharField(max_length=200, blank=True)
+    parent = models.CharField(max_length=100, blank=True)
+
+def import_tags_from_csv(apps, schema):
+    dir = os.path.dirname(__file__)
+    filename = os.path.join(dir, 'Tag_definitions.csv')
+    with open(filename, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        try:
+            #skip header row
+            next(reader)
+            for row in reader:
+                print(row)
+                tag = Tag(tag_name=row[0],
+                          display_name=row[1],
+                          caption=row[2],
+                          category=row[3],
+                          subcategory=row[4],
+                          parent=row[5]
+                          )
+                tag.save()
+        except csv.Error as e:
+            sys.exit('file %s, line %d: %s' % (filename, reader.line_num, e))
--- democracylab/settings.py
+++ democracylab/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'civictechprojects.apps.CivictechprojectsConfig',
+    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
