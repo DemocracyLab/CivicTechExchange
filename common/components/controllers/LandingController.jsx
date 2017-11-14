a632debf5f2abc9c792c55ec384ad178b00db022
Lowell Bander <lowellb@fb.com>
2017-11-13 21:55:13 -0800

    styling for supreme court banner

--- common/components/controllers/LandingController.jsx
+++ common/components/controllers/LandingController.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 
+function css(...classnames) {
+  const prefix = 'LandingController-';
+  return prefix + classnames.join(' ' + prefix);
+}
+
 class LandingController extends React.Component {
   render() {
     return (
       <div>
-        <div className="LandingController-supremeCourtBanner">
-          <div className="LandingController-landingTextContainer">
-            <p className="LandingController-landingText">
-              Use your skills to make a difference and change the world,
-              one project at a time.
+        <div className={css('supremeCourtBanner')}>
+          <div
+            className={css('landingTextContainer', 'centerer')}
+            >
+            <p
+              className={css('landingText', 'useYourSkills')}
+              >
+              Use your skills to make a difference and
+              change the world, one project at a time.
             </p>
-            <p className="LandingController-landingText">{"LET'S GET STARTED."}</p>
-            <button className="LandingController-landingText">Sign Up</button>
+            <p
+              className={css('landingText', 'useYourSkills')}
+              >
+                {"LET'S GET STARTED."}
+              </p>
+            <div
+              className={css('centerer', 'signUpContainer')}
+              >
+              <div
+                className={css('landingText', 'signUp')}
+                >
+                Sign Up
+              </div>
+            </div>
           </div>
         </div>
       </div>
     );
   }
+
 }
 
 export default LandingController;
