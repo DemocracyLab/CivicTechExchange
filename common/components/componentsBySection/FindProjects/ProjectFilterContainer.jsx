f390fa176c548fe348fdae1a60af68fe464aa8be
lowellbander <lowellb@fb.com>
2017-11-25 19:54:16 -0800

    flowify ProjectFilterContainer

--- common/components/componentsBySection/FindProjects/ProjectFilterContainer.jsx
+++ common/components/componentsBySection/FindProjects/ProjectFilterContainer.jsx
@@ -1,8 +1,10 @@
+// @flow
+
 import IssueAreasFilter from './IssueAreasFilter.jsx';
 import React from 'react';
 
-class ProjectFilterContainer extends React.PureComponent {
-  render() {
+class ProjectFilterContainer extends React.PureComponent<{||}> {
+  render(): React$Node {
     return (
       <div className="ProjectFilterContainer-root">
         Filter Search:
