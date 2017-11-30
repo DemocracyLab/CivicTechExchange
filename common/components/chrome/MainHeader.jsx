48c0e1535720b8c79c40d65aaba92bfff7309795
lowellbander <lowellb@fb.com>
2017-11-29 19:16:32 -0800

    main header styling

--- common/components/chrome/MainHeader.jsx
+++ common/components/chrome/MainHeader.jsx
@@ -15,16 +15,30 @@ class MainHeader extends React.PureComponent<Props> {
       <div className="MainHeader-root">
         <span
           onClick={() => this.props.onChangeSection(Section.Landing)}>
-          [DLAB LOGO]
+          <img
+            className="MainHeader-logo"
+            src="https://i.imgur.com/jvnaJWz.png"
+          />
         </span>
-        <span className="MainController-rightContent">
-          <span>About</span>
-          <span>Notifications</span>
-          <span>Messages</span>
-          <span>[PROF PIC]</span>
+        <span className="MainHeader-rightContent">
+          <span>
+            {this._renderLinks()}
+          </span>
+          <img
+            className="MainHeader-profilePicture"
+            src="https://avatars2.githubusercontent.com/u/3479724?s=400&u=d8619269a20955dd8df8215d1c41faad9b6fb62a&v=4"
+          />
         </span>
       </div>
     );
+  }
+
+  _renderLinks(): React$Node {
+    return [
+      'About',
+      'Notifications',
+      'Messages',
+    ].map(link => <span className="MainHeader-link" key={link}>{link}</span>);
   }
 }
 
