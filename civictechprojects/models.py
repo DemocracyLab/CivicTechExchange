092d7998f3e0cef1a24d0cab124ed05daf698c4a
lowellbander <lowellb@fb.com>
2017-11-29 19:57:48 -0800

    rel -> remote_field
    Deprecating old name as per https://docs.djangoproject.com/en/1.9/releases/1.9/#field-rel-changes
    
    No migration appears to be necessary for this change, as running
    
    `python manage.py makemigrations`
    
    simple returns
    
    `No changes detected`

--- civictechprojects/models.py
+++ civictechprojects/models.py
@@ -25,9 +25,9 @@ class Project(models.Model):
     project_creator = models.ForeignKey(Contributor)
     project_description = models.CharField(max_length=1000, blank=True)
     project_issue_area = TaggableManager(blank=True, through=TaggedIssueAreas)
-    project_issue_area.rel.related_name = "+"
+    project_issue_area.remote_field.related_name = "+"
     project_location = models.CharField(max_length=200)
     project_name = models.CharField(max_length=200)
     project_tags = TaggableManager(blank=True, through=TaggedTag)
-    project_tags.rel.related_name = "+"
+    project_tags.remote_field.related_name = "+"
     project_url = models.CharField(max_length=200, blank=True)
