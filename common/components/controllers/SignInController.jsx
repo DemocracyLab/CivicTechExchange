9e7bb45432fa22e911880966a30cab166ec670fe
lowellbander <lowellb@fb.com>
2018-01-10 17:57:50 -0800

    fleshing out signin page, will likely switch over to a standalone application

--- common/components/controllers/SignInController.jsx
+++ common/components/controllers/SignInController.jsx
@@ -1,24 +1,64 @@
 // @flow
 
 import DjangoCSRFToken from 'django-react-csrftoken'
+import NavigationDispatcher from '../stores/NavigationDispatcher.js';
 import React from 'react';
+import Section from '../enums/Section.js';
 
-class SignInController extends React.PureComponent<{||}> {
+type State = {|
+  username: string,
+  password: string,
+|}
+
+class SignInController extends React.PureComponent<{||}, State> {
+
+  constructor(): void {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
 
   render(): React$Node {
     return (
       <div>
         Welcome back. Log into your account.
-        <form action='/login/' method="post">
+        <form method="post" onSubmit={this._signIn.bind(this)}>
             <DjangoCSRFToken />
             Email:
-            <input type="text" name="username" />
+            <input
+              name="username"
+              onChange={e => this.setState({username: e.target.value})}
+              type="text"
+            />
             Password:
-            <input type="password" name="password" />
+            <input
+              name="password"
+              onChange={e => this.setState({password: e.target.value})}
+              type="password"
+            />
             <button type="submit">Login</button>
         </form>
       </div>
     );
+  }
+
+  _signIn(): void {
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", '/login/', true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.onreadystatechange = () => {
+      // $FlowFixMe
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 0) {
+        console.log('login success', xhr.responseText);
+        NavigationDispatcher.dispatch({
+          type: 'SET_SECTION',
+          section: Section.FindProjects,
+        });
+      }
+    };
+    xhr.send(this.state);
   }
 }
 
