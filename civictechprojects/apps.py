71129b2d74c32726e3b5ef09a74371844cc09329
Marlon Keating <marlonakeating@gmail.com>
2018-01-24 17:36:36 -0800

    Disable removing invalid tags

--- civictechprojects/apps.py
+++ civictechprojects/apps.py
@@ -6,5 +6,6 @@ class CivictechprojectsConfig(AppConfig):
 
     def ready(self):
         # Remove any tags that aren't in the canonical tag list
-        from .models import Project
-        Project.remove_tags_not_in_list()
+        # TODO: Fix so this doesn't break in production database
+        # from .models import Project
+        # Project.remove_tags_not_in_list()
