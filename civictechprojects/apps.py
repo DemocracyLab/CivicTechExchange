c6e69e1cec4de30a2e56ec865c3043edc7bf39b5
Marlon Keating <marlonakeating@gmail.com>
2018-01-24 17:41:05 -0800

    Comment out entire function to stop unexpected EOF effor

--- civictechprojects/apps.py
+++ civictechprojects/apps.py
@@ -4,8 +4,9 @@ from django.apps import AppConfig
 class CivictechprojectsConfig(AppConfig):
     name = 'civictechprojects'
 
-    def ready(self):
+    # def ready(self):
         # Remove any tags that aren't in the canonical tag list
         # TODO: Fix so this doesn't break in production database
         # from .models import Project
         # Project.remove_tags_not_in_list()
+
