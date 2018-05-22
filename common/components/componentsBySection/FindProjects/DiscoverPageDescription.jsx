df6f68e8f8cc8fa5a9c3e6d62e7b05c8ab9f6a90
Stas <pschnk@gmail.com>
2018-05-22 12:40:12 -0700

    add description to discover page

---
+++ common/components/componentsBySection/FindProjects/DiscoverPageDescription.jsx
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+
+class DiscoverPageDescription extends React.PureComponent<{||}> {
+
+  render(): React$Node {
+    return (
+      <p className="DiscoverPageDescription-root">
+        Welcome to DemocracyLab! Use the filters and search bar below to find tech-for-good projects in Seattle.
+      </p>
+    );
+  }
+}
+
+export default DiscoverPageDescription;
--- common/components/componentsBySection/FindProjects/ProjectSearchContainer.jsx
+++ common/components/componentsBySection/FindProjects/ProjectSearchContainer.jsx
@@ -3,12 +3,14 @@
 import ProjectFilterContainer from './ProjectFilterContainer.jsx';
 import ProjectTagContainer from './ProjectTagContainer.jsx';
 import ProjectSearchBar from './ProjectSearchBar.jsx';
+import DiscoverPageDescription from './DiscoverPageDescription.jsx';
 import React from 'react';
 
 class ProjectSearchContainer extends React.PureComponent<{||}> {
   render(): React$Node {
     return (
       <div>
+      	<DiscoverPageDescription />
         <ProjectSearchBar />
         <ProjectTagContainer />
         <ProjectFilterContainer />
