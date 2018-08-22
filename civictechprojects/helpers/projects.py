8411bb0fb403a052cecaadb8d26239bd829b9847
Peter Breen <pbreen@gmail.com>
2018-08-22 13:26:15 -0700

    change to project_organization instead of project_community, matching models.py

--- civictechprojects/helpers/projects.py
+++ civictechprojects/helpers/projects.py
@@ -6,16 +6,15 @@ from collections import Counter
 # Gets a breakdown of all issue/technology/position tags used in the given list of projects
 def projects_tag_counts(projects):
     # Get list of all tags used
-    # TODO: Add project stage and communities
-    issues, technologies, positions = [], [], []
+    issues, technologies, stage, organization, positions = [], [], [], [], []
     for project in projects:
         issues += project.project_issue_area.slugs()
         technologies += project.project_technologies.slugs()
         stage += project.project_stage.slugs()
-        community += project.project_community.slugs()
+        organization += project.project_organization.slugs()
 
         project_positions = ProjectPosition.objects.filter(position_project=project.id)
         positions += map(lambda position: position.position_role.slugs()[0], project_positions)
 
     # Get the counts of all the tags used
-    return merge_dicts(Counter(issues), Counter(technologies), Counter(stage), Counter(community), Counter(positions))
+    return merge_dicts(Counter(issues), Counter(technologies), Counter(stage), Counter(organization), Counter(positions))
