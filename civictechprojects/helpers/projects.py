2e0cef6a62075c575955388e40992c42e00b277f
Peter Breen <pbreen@gmail.com>
2018-08-28 12:46:24 -0700

    returns active tag counts for all categories

--- civictechprojects/helpers/projects.py
+++ civictechprojects/helpers/projects.py
@@ -1,20 +1,25 @@
-from civictechprojects.models import ProjectPosition
+from civictechprojects.models import Project, ProjectPosition
 from common.helpers.dictionaries import merge_dicts
 from collections import Counter
+from pprint import pprint
 
-
-# Gets a breakdown of all issue/technology/position tags used in the given list of projects
-def projects_tag_counts(projects):
-    # Get list of all tags used
+def projects_tag_counts():
+    projects = Project.objects.all()
     issues, technologies, stage, organization, positions = [], [], [], [], []
-    for project in projects:
-        issues += project.project_issue_area.slugs()
-        technologies += project.project_technologies.slugs()
-        stage += project.project_stage.slugs()
-        organization += project.project_organization.slugs()
+    if projects:
+        for project in projects:
+            issues += project.project_issue_area.slugs()
+            technologies += project.project_technologies.slugs()
+            stage += project.project_stage.slugs()
+            organization += project.project_organization.slugs()
 
-        project_positions = ProjectPosition.objects.filter(position_project=project.id)
-        positions += map(lambda position: position.position_role.slugs()[0], project_positions)
+            project_positions = ProjectPosition.objects.filter(position_project=project.id)
+            positions += map(lambda position: position.position_role.slugs()[0], project_positions)
+            # Get the counts of all the tags used
+            
+        return merge_dicts(Counter(issues), Counter(technologies), Counter(stage), Counter(organization), Counter(positions))
 
-    # Get the counts of all the tags used
-    return merge_dicts(Counter(issues), Counter(technologies), Counter(stage), Counter(organization), Counter(positions))
+
+            # sc = Counter(k['tag_name'] for k in projects if k.get('tag_name'))
+            # sc = Counter(projects)
+            # return sc
