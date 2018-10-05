a04733b785cf6163c681d6d32c2f1e4cf22af7df
Marlon Keating <marlonakeating@gmail.com>
2018-10-04 18:36:16 -0700

    Remove extraneous settings.py include

--- democracylab/settings.py
+++ democracylab/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 from distutils.util import strtobool
-# from .logging import CustomErrorHandler
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
