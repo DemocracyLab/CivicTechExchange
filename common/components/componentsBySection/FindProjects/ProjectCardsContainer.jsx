79f0dc5f33696750c7fe78d805ab103be9052fd6
Peter Breen <pbreen@gmail.com>
2018-10-29 10:31:25 -0700

    update no match found text

--- common/components/componentsBySection/FindProjects/ProjectCardsContainer.jsx
+++ common/components/componentsBySection/FindProjects/ProjectCardsContainer.jsx
@@ -41,7 +41,7 @@ class ProjectCardsContainer extends React.Component<{||}, State> {
     return !this.state.projects
       ? 'Loading projects ...'
       : this.state.projects.size === 0
-        ? 'No projects match the provided criteria.  Sign up for Alerts to be notified when projects matching this criteria are added.'
+        ? 'No projects match the provided criteria.  Sign up for an alert to be notified when matching projects are added or try a different set of filters.'
         : this.state.projects.map(
           (project, index) =>
             <ProjectCard
