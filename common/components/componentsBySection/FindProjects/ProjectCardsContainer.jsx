6b1e2b606b5aceccbf81888ba3ba3ee6c53867a0
Peter Breen <pbreen@gmail.com>
2018-11-08 10:53:14 -0800

    add padding/margin control classes to container

--- common/components/componentsBySection/FindProjects/ProjectCardsContainer.jsx
+++ common/components/componentsBySection/FindProjects/ProjectCardsContainer.jsx
@@ -27,8 +27,8 @@ class ProjectCardsContainer extends React.Component<{||}, State> {
 
   render(): React$Node {
     return (
-      <div className="ProjectCardContainer col-12 col-md-9">
-        <div className="container-fluid">
+      <div className="ProjectCardContainer col-12 col-md-9 p-0 m-0">
+        <div className="container-fluid pl-0 pr-0">
           <div className="row">
             {this._renderCards()}
           </div>
