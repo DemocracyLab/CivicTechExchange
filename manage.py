f1e9c80e77402763207a71fc11e7013ca8c8f9cb
Marlon Keating <marlonakeating@gmail.com>
2019-01-07 13:30:28 -0800

    Add hook for custom manage.py commands

--- manage.py
+++ manage.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+from management.commands import execute_democracylab_management_command
+
+
+def execute_custom_commands(argv=None):
+    return execute_democracylab_management_command(argv)
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "democracylab.settings")
@@ -19,4 +24,7 @@ if __name__ == "__main__":
                 "forget to activate a virtual environment?"
             )
         raise
-    execute_from_command_line(sys.argv)
+    if not execute_custom_commands(sys.argv):
+        execute_from_command_line(sys.argv)
+
+
---
+++ management/email_reminders.py
@@ -0,0 +1,2 @@
+def execute_email_reminders(argv=None):
+    print("TODO: Email reminders")
