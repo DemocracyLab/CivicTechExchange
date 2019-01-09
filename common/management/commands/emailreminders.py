31b55aec7f6dac361f375f7b2e50b6db3c1a222c
Marlon Keating <marlonakeating@gmail.com>
2019-01-09 09:44:21 -0800

    Calculate when to send volunteer application email reminder

--- common/management/commands/emailreminders.py
+++ common/management/commands/emailreminders.py
@@ -1,15 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
-#TODO: Make this configurable
+# TODO: Make this configurable
+# This array specifies how many days we should space our reminder emails.  In this case, the first reminder comes after
+# two days, and then repeats every seven days
 reminder_interval_days = [2, 7]
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print('Made it to emailreminders')
         from civictechprojects.models import VolunteerRelation
         pending_volunteer_applications = VolunteerRelation.objects.filter(is_approved=False)
-        # for volunteer in pending_volunteer_applications:
-            # days_since_last_reminder = (timezone.now - volunteer.last_reminder_date).days()
-            # print(days_since_last_reminder, " days since last reminder for Volunteer Relation ", volunteer.id)
+        for volunteer in pending_volunteer_applications:
+            if time_for_reminder(volunteer):
+                print("Pending application ", volunteer.__str__(), " should get a reminder")
+
+
+def time_for_reminder(volunteer):
+    time_of_application_or_reminder = volunteer.last_reminder_date or volunteer.application_date
+    days_since_last_reminder = (timezone.now() - time_of_application_or_reminder).days
+    days_to_next_reminder = reminder_interval_days[min(volunteer.reminder_count, len(reminder_interval_days) - 1)]
+    return days_since_last_reminder >= days_to_next_reminder
