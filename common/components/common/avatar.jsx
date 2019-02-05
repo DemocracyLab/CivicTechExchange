b42515ab3f979e9d0de4ddf339c2c7ea83927bd6
justBanks <Darryl.Banks@gmail.com>
2019-02-04 17:48:10 -0800

    readability tweak

--- common/components/common/avatar.jsx
+++ common/components/common/avatar.jsx
@@ -13,9 +13,11 @@ class Avatar extends React.PureComponent<Props> {
   render(): React$Node {
     const user = this.props.user;
     return (
-      user.user_thumbnail ?
-        <img className="upload_img upload_img_bdr VolunteerCard-img" src={user.user_thumbnail.publicUrl} /> :
-        <span className="Icon-container"><Person className="PersonIcon" style={{ fontSize: this.props.size }} /></span>
+      user.user_thumbnail
+        ? <img className="upload_img upload_img_bdr VolunteerCard-img" src={user.user_thumbnail.publicUrl} />
+        : (<span className="Icon-container">
+            <Person className="PersonIcon" style={{ fontSize: this.props.size }} />
+          </span>)
     );
   }
 }
