9fffd911b564f9f0533c46c5344677dd7e3c6686
Marlon Keating <marlonakeating@gmail.com>
2019-02-06 21:20:41 -0800

    Show description metadata on find projects page

--- common/components/controllers/FindProjectsController.jsx
+++ common/components/controllers/FindProjectsController.jsx
@@ -7,7 +7,9 @@ import ProjectSearchContainer from '../componentsBySection/FindProjects/ProjectS
 import ProjectFilterContainer from '../componentsBySection/FindProjects/ProjectFilterContainer.jsx';
 import {FindProjectsArgs} from "../stores/ProjectSearchStore.js";
 import SplashScreen from "../componentsBySection/FindProjects/SplashScreen.jsx";
+import Headers from "../common/Headers.jsx";
 import urls from "../utils/url.js";
+import cdn from "../utils/cdn.js";
 import React from 'react';
 import _ from 'lodash'
 
@@ -36,6 +38,11 @@ class FindProjectsController extends React.PureComponent<{||}, State> {
   render(): React$Node {
     return (
       <React.Fragment>
+        <Headers
+          title="DemocracyLab"
+          description="Optimizing the connection between skilled volunteers and tech-for-good projects"
+          thumbnailUrl={cdn.image("dl_logo.png")}
+        />
         {this.state.showSplash ? <SplashScreen onClickFindProjects={this._onClickFindProjects.bind(this)}/> : null}
         <div className="FindProjectsController-root container">
           <ProjectSearchContainer />
