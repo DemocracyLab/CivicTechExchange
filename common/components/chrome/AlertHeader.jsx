66eb09e91b035fce6726b88b34c101c73274e74f
Marlon Keating <marlonakeating@gmail.com>
2019-02-26 10:02:36 -0800

    Close alert header when user clicks on X

--- common/components/chrome/AlertHeader.jsx
+++ common/components/chrome/AlertHeader.jsx
@@ -6,18 +6,32 @@ type Props = {|
   onClickFindProjects: () => void
 |};
 
+type State = {|
+  showHeader: boolean
+|};
+
+// Put string html content into a format that dangerouslySetInnerHTML accepts
 function unescapeHtml(html: string): string {
   let escapeEl = document.createElement('textarea');
   escapeEl.innerHTML = html;
   return escapeEl.textContent;
 }
 
-class AlertHeader extends React.PureComponent<Props> {
-  render(): React$Node {
-    return (
+class AlertHeader extends React.PureComponent<Props, State> {
+  constructor(): void {
+    super();
+    this.state = {showHeader: true};
+  }
+  
+  hideHeader(): void {
+    this.setState({showHeader: false});
+  }
+  
+  render(): ?React$Node {
+    return this.state.showHeader && (
       <div className="AlertHeader-root">
         <div className="AlertHeader-text" dangerouslySetInnerHTML={{__html: unescapeHtml(window.HEADER_ALERT)}}/>
-        <div className="AlertHeader-close">
+        <div className="AlertHeader-close" onClick={() => this.hideHeader()}>
           X
         </div>
       </div>
