09db6db7bd3bf705e1bba489524d93ae505c1cd8
Marlon Keating <marlonakeating@gmail.com>
2019-03-14 15:14:09 -0700

    Add framework for constructing html emails

--- democracylab/emails.py
+++ democracylab/emails.py
@@ -1,11 +1,54 @@
 from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
+from django.template import loader, Template, Context
+from enum import Enum
+from html.parser import unescape
 from common.models.tags import Tag
 from democracylab.models import Contributor
 from civictechprojects.models import VolunteerRelation
 from common.helpers.constants import FrontEndSection
 from common.helpers.front_end import section_url
+
+
+class EmailSection(Enum):
+    Header = 'Header'
+    Button = 'Button'
+    Paragraph = 'Paragraph'
+
+
+class HtmlEmailTemplate:
+    base_template = loader.get_template('html_email_frame.html')
+    section_templates = {
+        EmailSection.Header: loader.get_template('html_email_header.html'),
+        EmailSection.Button: loader.get_template('html_email_button.html'),
+        EmailSection.Paragraph: loader.get_template('html_email_paragraph.html')
+    }
+
+    def __init__(self):
+        self.sections = []
+        self.hydrated_template = None
+
+    def add(self, section_type, section_content):
+        self.sections.append(HtmlEmailTemplate.section_templates[section_type].render(section_content))
+        return self
+
+    def header(self, text):
+        return self.add(EmailSection.Header, {'text': text})
+
+    def paragraph(self, text):
+        return self.add(EmailSection.Paragraph, {'text': text})
+
+    def button(self, url, text):
+        return self.add(EmailSection.Button, {'url': url, 'text': text})
+
+    def render(self, email_msg, context):
+        if self.hydrated_template is None:
+            sections_text = ''.join(self.sections)
+            self.hydrated_template = Template(HtmlEmailTemplate.base_template.render({"content": sections_text}))
+        email_msg.content_subtype = "html"
+        email_msg.body = unescape(self.hydrated_template.render(Context(context)))
+        return email_msg
 
 
 def send_verification_email(contributor):
