3fbeded85eabe981a2e604f452dc12440bd45945
Peter Breen <pbreen@gmail.com>
2019-03-15 11:33:27 -0700

    change filter key generation to avoid collision error with non-unique display names across categories

--- common/components/componentsBySection/FindProjects/Filters/RenderFilterCategory.jsx
+++ common/components/componentsBySection/FindProjects/Filters/RenderFilterCategory.jsx
@@ -105,9 +105,9 @@ class RenderFilterCategory<T> extends React.PureComponent<Props<T>, State> {
   _renderFilterList(data) {
     //this function renders individual clickable filter items regardless of category or subcategory status
     let sortedTags = Object.values(data).map((tag) =>
-      <li key={tag.category + '-' + tag.display_name} className="ProjectFilterContainer-list-item">
-          <input type="checkbox" id={tag.category + '-' + tag.display_name} checked={this.props.checkEnabled(tag)} onChange={() => this.props.selectOption(tag)}></input>
-          <label htmlFor={tag.category + '-' + tag.display_name}>
+      <li key={tag.category + '-' + tag.tag_name} className="ProjectFilterContainer-list-item">
+          <input type="checkbox" id={tag.category + '-' + tag.tag_name} checked={this.props.checkEnabled(tag)} onChange={() => this.props.selectOption(tag)}></input>
+          <label htmlFor={tag.category + '-' + tag.tag_name}>
             <span className="ProjectFilterContainer-list-item-name">{tag.display_name}</span> <span className="ProjectFilterContainer-list-item-count">{this.props.checkEnabled(tag) ? <i className={GlyphStyles.Check}></i> : tag.num_times}</span>
           </label>
       </li>
