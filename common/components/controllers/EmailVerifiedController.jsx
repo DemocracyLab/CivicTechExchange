fe079fc35613507794768389e63dcc616140ba81
Harman Bains <bains.hmn@gmail.com>
2019-03-16 14:48:50 -0700

    initial layout of EmailVerified page

--- common/components/controllers/EmailVerifiedController.jsx
+++ common/components/controllers/EmailVerifiedController.jsx
@@ -1,6 +1,10 @@
 // @flow
 
 import React from 'react';
+import {Button} from 'react-bootstrap';
+import cdn from "../utils/cdn.js";
+import url from '../utils/url.js'
+import Section from '../enums/Section.js'
 
 type Props = {|
 
@@ -16,10 +20,17 @@ class EmailVerifiedController extends React.Component<Props, State> {
   
   render(): React$Node {
     return (
-      <div className="LogInController-root">
-        <div className="LogInController-greeting">
-          You have verified your email!
-        </div>
+      <div className="EmailVerifiedController-root">
+        <img
+          className="EmailVerifiedController-logo"
+          src="/static/images/projectPlaceholder.png"
+          //src={cdn.image("dl_logo.png")}
+        />
+        <h2>Your email has been verified</h2>
+        <p className="EmailVerifiedController-subhead">Get started by finding tech-for-good projects that match your skill and interests.</p>
+        <a className="EmailVerified-find-projects-btn btn btn-default"href={url.section(Section.FindProjects, {showSplash: 1})}>
+        FIND PROJECTS
+        </a>
       </div>
     );
   }
