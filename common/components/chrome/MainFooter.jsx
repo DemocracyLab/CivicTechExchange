41fdcfa31b3c446a9596a3bb80e8073ce9bf56f3
Peter Breen <pbreen@gmail.com>
2019-04-04 12:31:52 -0700

    restyles footer to use Boostrap layout

--- common/components/chrome/MainFooter.jsx
+++ common/components/chrome/MainFooter.jsx
@@ -6,15 +6,15 @@ import Section from "../enums/Section.js";
 import Sponsors, {SponsorMetadata} from "../utils/Sponsors.js";
 
 class MainFooter extends React.PureComponent<{||}> {
-  
+
   constructor(): void {
     super();
   }
-  
+
   render(): React$Node {
     return (
-      <div className="MainFooter-footer">
-        <div className="MainFooter-item">
+      <div className="MainFooter-footer container">
+        <div className="MainFooter-item col-xs-12 col-md-6">
           <h2>Sponsors Make It Possible</h2>
           <p>Support the acceleration of social change</p>
           <a className="EmailVerified-find-projects-btn btn btn-default" href={url.section(Section.PartnerWithUs)}>
@@ -25,13 +25,13 @@ class MainFooter extends React.PureComponent<{||}> {
       </div>
     );
   }
-  
+
   _renderSponsors(): ?Array<React$Node>  {
     const sponsors: $ReadOnlyArray<SponsorMetadata> = Sponsors.list();
     if(sponsors) {
       return sponsors.map( (sponsor: SponsorMetadata, i:number) => {
         return (
-          <div key={i} className="MainFooter-sponsor MainFooter-item">
+          <div key={i} className="MainFooter-sponsor MainFooter-item col-xs-12 col-md-6">
             <div>
               <a href={sponsor.url} target="_blank" rel="noopener noreferrer">
                 <img src={sponsor.thumbnailUrl}/>
