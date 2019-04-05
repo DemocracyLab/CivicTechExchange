e3a0fb79b2cbcc777da1021c7954224e32320cdc
Marlon Keating <marlonakeating@gmail.com>
2019-04-04 19:27:05 -0700

    Footer monitors page navigation and displays itself accordingly

--- common/components/controllers/MainController.jsx
+++ common/components/controllers/MainController.jsx
@@ -6,19 +6,12 @@ import UniversalDispatcher from '../stores/UniversalDispatcher.js';
 import React from 'react';
 import FlashMessage from '../chrome/FlashMessage.jsx';
 import MainFooter from "../chrome/MainFooter.jsx";
-import Section from "../enums/Section.js";
 import url from '../../components/utils/url.js'
-import _ from 'lodash';
 
 type State = {|
   headerHeight: number,
   currentSection: ?string
 |};
-
-const sectionsToShowFooter: $ReadOnlyArray<string> = [
-  Section.FindProjects,
-  Section.AboutProject
-];
 
 class MainController extends React.Component<{||}, State> {
   constructor() {
@@ -58,7 +51,7 @@ class MainController extends React.Component<{||}, State> {
       <MainHeader key='main_header' onMainHeaderHeightChange={this._mainHeaderHeightChange.bind(this)}/>,
       <FlashMessage key='flash_message'/>,
       <SectionController key='section_controller' headerHeight={this.state.headerHeight}/>,
-      this.state.currentSection && (_.includes(sectionsToShowFooter, this.state.currentSection)) ? <MainFooter/> : null
+      <MainFooter/>
     ];
   }
 }
