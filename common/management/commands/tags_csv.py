4b56d64e3103a0f3d1e5d2c26fa7ecd7eaba7480
justBanks <Darryl.Banks@gmail.com>
2019-04-09 20:40:12 -0700

    console command prints csv list

---
+++ common/management/commands/tags_csv.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from common.models.tags import Tag
+from string import Template
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+    	print("Canonical Name(lowercase + '-'),Display Name(shown on UI),Caption (Hover-over text in UI),Category,Subcategory,Parent (Use Canonical Name)")
+    	for tag in Tag.objects.all():
+    		fields = (tag.tag_name,tag.display_name,tag.caption,tag.category,tag.subcategory,tag.parent)
+    		print(','.join(fields))
