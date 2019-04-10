d62437722d3b0b6c2c6e556e4aa46b4703aaf476
justBanks <Darryl.Banks@gmail.com>
2019-04-09 20:44:57 -0700

    cleanup

--- common/management/commands/tags_csv.py
+++ common/management/commands/tags_csv.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from common.models.tags import Tag
-from string import Template
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
