5318c402c8af7481b5509a079224aa42b9c57bea
Peter Breen <pbreen@gmail.com>
2019-04-30 11:14:34 -0700

    Our Mission and Our Vision content complete

--- common/components/controllers/AboutUsController.jsx
+++ common/components/controllers/AboutUsController.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import _ from 'lodash'
 import ProjectAPIUtils from '../utils/ProjectAPIUtils.js';
 import type {ProjectDetailsAPIData} from '../utils/ProjectAPIUtils.js';
+import cdn from '../utils/cdn.js';
 // TODO: Create metrics event for this page, then import metrics and use it
 // import metrics from "../utils/metrics.js";
 
@@ -35,8 +36,7 @@ class AboutUsController extends React.PureComponent<{||}, State> {
 
   render(): $React$Node {
     return (
-      <div className="container">
-        <p>hello!</p>
+      <div className="container about-us-root">
         {this._ourMission()}
         {this._ourVision()}
         {this._ourValues()}
@@ -45,18 +45,59 @@ class AboutUsController extends React.PureComponent<{||}, State> {
     )
   }
 
+//cdn.image("filename.png")
+// ChallengeIcon.png
+// CommunityIcon.png
+// CoreValuesBG.png
+// EncourageTransparency.png
+// InnovateIcon.png
+// OurMissionBG.png
+// OurVisionBG.png
+
   _ourMission() {
-    return (<div>Our Mission</div>)
+    return (
+      <div className="about-us-mission"
+      style={this.bgStyle('OurMissionBG.png')}>
+        <div className="about-us-content">
+          <h2>Our Mission</h2>
+          <p>We connect people who create technology for public good with talent and resources to achieve their vision</p>
+        </div>
+      </div>
+    )
   }
   _ourVision() {
-    return (<div>Our Vision</div>)
+    return (
+      <div className="about-us-vision"
+      style={this.bgStyle('OurVisionBG.png')}>
+        <div className="about-us-content">
+          <h2>Our Vision</h2>
+          <p> At DemocracyLab, we envision a world where everyone who wants to make the world a better place has the [power/ability/opportunity/tools...] to do so.</p>
+        </div>
+      </div>
+    )
   }
   _ourValues() {
-    return (<div>Our Values</div>)
+    return (
+      <div className="row about-us-values">
+        Our Values
+      </div>
+    )
   }
   _ourTeam() {
-    return (this.state.aboutUs ? <div>Our Team</div> : <div>Loading our team information...</div>)
+    return (this.state.aboutUs ?
+      <div className="row about-us-team">
+        Our Team
+      </div> : <div>Loading our team information...</div>)
   }
+
+  bgStyle(filename) {
+    //only set the background image here because it's a CDN link
+    let bgImg = cdn.image(filename);
+    let style= {
+      backgroundImage: `url(${bgImg})`,
+     }
+    return style;
+   }
 }
 
 export default AboutUsController;
