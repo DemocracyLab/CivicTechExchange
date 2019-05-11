8d21138835af3d7fe6fcbff21ed754f9fec62d8a
Joyce Liao <joyliao07@gmail.com>
2019-05-11 11:57:59 -0700

    SCSS file setup

--- common/components/controllers/ThankYouController.jsx
+++ common/components/controllers/ThankYouController.jsx
@@ -12,20 +12,23 @@ class ThankYouController extends React.Component<{||}> {
   render(): React$Node {
     return (
       <div className="ThankYou-root">
-        <div>
-          picture
+
+        <div className="ThankYou-leftImage">
           <img src={cdn.image(Images.THANK_YOU)}/>
         </div>
 
-        <div>
-          <h2>Thanks for your support!</h2>
-          <p>Your donation supports the valuable work of our organization. Thank you for your donation.</p>
-        </div>
+        <div className="ThankYou-rightColumn">
 
-        <div>
-          <h3>Share with your friends</h3>
-        </div>
+          <div className="ThankYou-message">
+            <h2>Thanks for your support!</h2>
+            <p>Your donation supports the valuable work of our organization. Thank you for your donation.</p>
+          </div>
 
+          <div className="ThankYou-share">
+            <h3>Share with your friends</h3>
+          </div>
+
+        </div>
       </div>
     );
   }
