724cdad4e4e46bc83d120f2f35fbd6aaf9f476e4
Peter Breen <pbreen@gmail.com>
2019-05-15 10:30:39 -0700

    bio modal styles

--- common/components/componentsBySection/AboutUs/BioModal.jsx
+++ common/components/componentsBySection/AboutUs/BioModal.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import {Modal, Button} from 'react-bootstrap';
-import GlyphStyles from '../../utils/glyphs.js';
+import {Glyph, GlyphStyles, GlyphSizes} from '../../utils/glyphs.js';
 
 type Props = {|
   showModal: boolean,
@@ -38,20 +38,18 @@ class BioModal extends React.PureComponent<Props, State> {
   render(): React$Node {
     return this.props.person && (
       <div>
-          <Modal show={this.state.showModal} onHide={this.closeModal} bsSize={this.props.size}>
+          <Modal show={this.state.showModal} onHide={this.closeModal} bsSize={this.props.size} className="bio-modal-root">
               <Modal.Header>
-                <div className="bio-nametitle-container">
-                  <h4 className="bio-name">{this.props.person.first_name} {this.props.person.last_name}</h4>
-                  <h5 className="bio-title">{this.props.title}</h5>
+                <div className="bio-modal-nametitle-container">
+                  <h4 className="bio-modal-name">{this.props.person.first_name} {this.props.person.last_name}</h4>
+                  <h5 className="bio-modal-title">{this.props.title}</h5>
                 </div>
-                <i className={GlyphStyles.Close} onClick={this.closeModal}></i>
+                <i className={Glyph(GlyphStyles.Close, GlyphSizes.X2)} onClick={this.closeModal}></i>
               </Modal.Header>
               <Modal.Body style={{whiteSpace: "pre-wrap"}}>
+                <h5 className="bio-modal-about">About</h5>
                 <p>{this.props.person.about_me}</p>
               </Modal.Body>
-              <Modal.Footer>
-                  <Button onClick={this.closeModal}>Close</Button>
-              </Modal.Footer>
           </Modal>
       </div>
     );
