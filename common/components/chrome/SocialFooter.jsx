49ed33ce2a0cc4bea12b811f223b809a2aad128b
Peter Breen <pbreen@gmail.com>
2019-06-17 11:45:17 -0700

    initial setup

---
+++ common/components/chrome/SocialFooter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SocialFooter extends React.Component {
+
+  render() {
+    return (
+      <div className="SocialFooter-root container">
+        Footer goes here
+      </div>
+    )
+  }
+}
+
+export default SocialFooter
