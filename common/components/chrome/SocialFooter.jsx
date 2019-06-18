b036a1dc9c44dfcc5ef999bcd358f3ec2ed68f8e
Peter Breen <pbreen@gmail.com>
2019-06-18 12:04:15 -0700

    add more template sections after design call

--- common/components/chrome/SocialFooter.jsx
+++ common/components/chrome/SocialFooter.jsx
@@ -2,10 +2,43 @@ import React from 'react';
 
 class SocialFooter extends React.Component {
 
+  _dlCallToActions() {
+    return (
+      <p>Welcome to DemocracyLab</p>
+    )
+  }
+  _socialLinks() {
+    return (
+      <p>Join us on social media somewhere....</p>
+    )
+  }
+  _copyrightInfo() {
+    return (
+      <p>Copyright &copy; 2019 DemocracyLab. All Rights Reserved.</p>
+    )
+  }
+  _footerNavigation() {
+    return (
+      <p>Footer navigation links go here</p>
+    )
+  }
+
+  _newsletterSignup() {
+    return (
+      <p>Sign up for our newsletter!</p>
+    )
+  }
+
   render() {
     return (
-      <div className="SocialFooter-root container">
-        Footer goes here
+      <div className="SocialFooter-root container-fluid">
+        <div className="bounded-content">
+          {this._dlCallToActions}
+          {this._socialLinks}
+          {this._copyrightInfo}
+          {this._footerNavigation}
+          {this._newsletterSignup}
+        </div>
       </div>
     )
   }
