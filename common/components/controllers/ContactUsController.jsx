fef1cd54be9474efbb5bebb94e30a26c99d8372d
Peter Breen <pbreen@gmail.com>
2019-06-27 12:59:28 -0700

    add social media links

--- common/components/controllers/ContactUsController.jsx
+++ common/components/controllers/ContactUsController.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import Headers from "../common/Headers.jsx";
+import {Glyph, GlyphStyles} from "../utils/glyphs.js"
 
 class ContactUsController extends React.PureComponent<{||}> {
 
@@ -23,7 +24,14 @@ class ContactUsController extends React.PureComponent<{||}> {
         {this._renderHeader()}
          <div className="container contact-us-root">
            <h1>Contact Us</h1>
-           <p>Words about contacting us with some links go here</p>
+           <p>Want to contact DemocracyLab? We'd love to hear from you. Send us an email at <a href="mailto:hello@democracylab.org">hello@democracylab.org</a> and we'll get back to you.</p>
+           <p>Interested in following us elsewhere? You can find us here:</p>
+           <ul className="fa-ul">
+             <li><a href="https://www.facebook.com/democracylaborg"><span className="fa-li"><i className={Glyph(GlyphStyles.FacebookSquare)}></i></span> Facebook</a></li>
+             <li><a href="https://www.linkedin.com/company/democracylab"><span className="fa-li"><i className={Glyph(GlyphStyles.LinkedIn)}></i></span> LinkedIn</a></li>
+             <li><a href="https://medium.com/democracylab-org"><span className="fa-li"><i className={Glyph(GlyphStyles.Medium)}></i></span> Medium</a></li>
+             <li><a href="https://twitter.com/democracylab"><span className="fa-li"><i className={Glyph(GlyphStyles.TwitterSquare)}></i></span> Twitter</a></li>
+           </ul>
          </div>
        </React.Fragment>
      )
