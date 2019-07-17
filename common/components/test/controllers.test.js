cd9498db14108c57bc5383f899751bd946337b81
justBanks <Darryl.Banks@gmail.com>
2019-07-16 20:13:43 -0700

    reformat code

--- common/components/test/controllers.test.js
+++ common/components/test/controllers.test.js
@@ -7,12 +7,12 @@ import MainController from '../controllers/MainController'
 describe('controllers', () => {
 
   it('should render without crashing', () => {
-  	//fetch.resetMocks() <--Use before making assertions on the arguments passed to fetch()
-  	//ProjectSearchStore calls fetch() as a Promise
-		fetch.mockResponse(() => new Promise(resolve => {})
-																.then(response => ({body: mockAPI.projects})))
+    //fetch.resetMocks() <--Use before making assertions on the arguments passed to fetch()
+    //ProjectSearchStore calls fetch() as a Promise
+    fetch.mockResponse(() => new Promise(resolve => {})
+                                .then(response => ({body: mockAPI.projects})));
 
-		ReactDOM.render(<MainController />, document.createElement('div'))
-  })
+    ReactDOM.render(<MainController />, document.createElement('div'));
+  });
 
-})
+});
--- common/components/test/utils.test.js
+++ common/components/test/utils.test.js
@@ -3,12 +3,12 @@ import async from '../utils/async'
 describe('utils', () => {
 
   test('async.js', () => {
-  	expect.hasAssertions()
-  	const readyFunc = () => true
-  	const doVoidFunc = jest.fn()
+    expect.hasAssertions()
+    const readyFunc = () => true;
+    const doVoidFunc = jest.fn();
 
-  	async.doWhenReady(readyFunc, doVoidFunc, 1)
-  	expect(doVoidFunc).toBeCalled()
-  })
+    async.doWhenReady(readyFunc, doVoidFunc, 1);
+    expect(doVoidFunc).toBeCalled();
+  });
 
-})
+});
