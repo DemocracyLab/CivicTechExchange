3f37e274dd3094163c5ed8d41ec2b95637ef2eed
Marlon Keating <marlonakeating@gmail.com>
2019-07-20 19:10:57 -0700

    Refresh alerts after navigating to new section

--- common/components/chrome/AlertHeader.jsx
+++ common/components/chrome/AlertHeader.jsx
@@ -1,11 +1,13 @@
 // @flow
 
 import React from 'react';
+import { Container } from 'flux/utils';
 import {Glyph,GlyphStyles, GlyphSizes} from "../utils/glyphs.js";
 import {Dictionary} from "../types/Generics.jsx";
 import CurrentUser from "../utils/CurrentUser.js";
 import url from "../utils/url.js";
 import Section from "../enums/Section.js";
+import NavigationStore from "../stores/NavigationStore.js";
 import moment from 'moment';
 import _ from 'lodash'
 
@@ -42,7 +44,7 @@ function unescapeHtml(html: string): string {
   return escapeEl.textContent;
 }
 
-class AlertHeader extends React.PureComponent<Props, State> {
+class AlertHeader extends React.Component<Props, State> {
   constructor(): void {
     super();
     const alertConfigurations: $ReadOnlyArray<AlertConfiguration> = [
@@ -82,6 +84,25 @@ class AlertHeader extends React.PureComponent<Props, State> {
       alertConfigurations: alertConfigurations
     };
   }
+  
+  static getStores(): $ReadOnlyArray<FluxReduceStore> {
+    return [NavigationStore];
+  }
+  
+  static calculateState(prevState: State): State {
+    // Re-calculate the current alert if we navigate to a different section
+    return {
+      currentAlert: null
+    };
+  }
+  
+  componentWillUpdate(nextProps: Props, nextState: State) {
+    if(!nextState.currentAlert) {
+      this.setState({
+        currentAlert: this.getCurrentAlert(this.state.alertConfigurations)
+      });
+    }
+  }
 
   hideHeader(): void {
     url.removeArgs(_.keys(AlertMessages));
@@ -116,7 +137,7 @@ class AlertHeader extends React.PureComponent<Props, State> {
   }
   
   render(): ?React$Node {
-    return this.state.showHeader && (
+    return this.state.showHeader && this.state.currentAlert && (
       <div className="AlertHeader-root">
         {this._renderCurrentAlert()}
         <div className="AlertHeader-close" onClick={() => this.hideHeader()}>
@@ -163,4 +184,4 @@ class AlertHeader extends React.PureComponent<Props, State> {
     );
   }
 }
-export default AlertHeader;
+export default Container.create(AlertHeader);
