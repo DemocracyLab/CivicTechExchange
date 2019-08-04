5f07a355d50c5bc9a1d72b6679445ffa311b4a4b
Marlon Keating <marlonakeating@gmail.com>
2019-08-04 12:22:29 -0700

    Contacting volunteers doesn't send to volunteers awaiting approval

--- civictechprojects/models.py
+++ civictechprojects/models.py
@@ -508,4 +508,4 @@ class VolunteerRelation(Archived):
 
     @staticmethod
     def get_by_project(project, active=True):
-        return VolunteerRelation.objects.filter(project_id=project.id, deleted=not active)
+        return VolunteerRelation.objects.filter(project_id=project.id, is_approved=active, deleted=not active)
