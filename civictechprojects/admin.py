7cd524df026dc793f0ae0c55b443c1d13c085f08
Matthew Meyers <matthew.gregory.meyers@gmail.com>
2019-08-16 10:54:12 -0700

    Adds groups and events to admin dashboard

--- civictechprojects/admin.py
+++ civictechprojects/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-from .models import Project, UserAlert, VolunteerRelation
+from .models import Project, Group, Event, ProjectRelationship, UserAlert, VolunteerRelation
 
 admin.site.register(Project)
+admin.site.register(ProjectRelationship)
+admin.site.register(Group)
+admin.site.register(Event)
 admin.site.register(UserAlert)
 admin.site.register(VolunteerRelation)
