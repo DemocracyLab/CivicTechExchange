9465e22bd588004429a77e7882ee428c03f93c9a
Marlon Keating <marlonakeating@gmail.com>
2019-09-27 10:14:33 -0700

    Update mission statement on About Us page

--- common/components/controllers/AboutUsController.jsx
+++ common/components/controllers/AboutUsController.jsx
@@ -72,7 +72,7 @@ class AboutUsController extends React.PureComponent<{||}, State> {
       style={cdn.bgImage('OurMissionBGoverlay.jpg')}>
         <div className="about-us-content container">
           <h1>Mission</h1>
-          <p>Empower a community of people and projects that use technology to advance the public good.</p>
+          <p>DemocracyLab is a nonprofit organization.  Our mission is to empower people who use technology to advance the public good.</p>
         </div>
       </div>
     )
