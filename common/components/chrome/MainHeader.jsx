925d136fa58f0623d6f52941928fa9d855241b68
justBanks <Darryl.Banks@gmail.com>
2019-10-22 11:57:13 -0700

    decode ampersands in LinkedIn avatar url (DLAB_GLOBAL_CONTEXT encoded by browser)

--- common/components/chrome/MainHeader.jsx
+++ common/components/chrome/MainHeader.jsx
@@ -360,7 +360,7 @@ class MainHeader extends React.Component<{||}, State > {
   _renderIcon(): React$Node {
     return (
       !_.isEmpty(CurrentUser.userImgUrl()) ?
-        <img className="SubHeader-userImg" src={CurrentUser.userImgUrl()} /> :
+        <img className="SubHeader-userImg" src={CurrentUser.userImgUrl().replace(/&amp;/g, '&')} /> :
         <Person className="SubHeader-userIcon" />
     );
   }
