0dfd65332936f8d1f6a62526558dd8f288b7d500
Peter Breen <pbreen@gmail.com>
2019-10-30 11:44:09 -0700

    update btn-default to btn-secondary

--- common/components/controllers/EmailVerifiedController.jsx
+++ common/components/controllers/EmailVerifiedController.jsx
@@ -9,7 +9,7 @@ class EmailVerifiedController extends React.Component<{||}> {
   constructor(): void {
     super();
   }
-  
+
   render(): React$Node {
     return (
       <div className="EmailVerifiedController-root">
@@ -19,7 +19,7 @@ class EmailVerifiedController extends React.Component<{||}> {
         <div className="EmailVerifiedController-greeting">
           <h3>Your email has been verified</h3>
           <p>Get started by finding tech-for-good projects that match your skill and interests.</p>
-          <a className="EmailVerified-find-projects-btn btn btn-default" href={url.section(Section.FindProjects, {showSplash: 1})}>
+          <a className="EmailVerified-find-projects-btn btn-secondary" href={url.section(Section.FindProjects, {showSplash: 1})}>
             FIND PROJECTS
           </a>
         </div>
