e06400eceefc487c371c226b5463a078ed2ad468
Marlon Keating <marlonakeating@gmail.com>
2019-11-09 10:32:21 -0800

    Fetch project commits based on project's public github url

--- common/helpers/github.py
+++ common/helpers/github.py
@@ -1,2 +1,27 @@
-def fetch_github_repository_info():
-    pass
+from urllib import parse as urlparse
+import requests
+from pprint import pprint
+
+
+def fetch_github_repository_info(repo_url):
+    # TODO: Use api key so we can get a usable rate limit
+    response = requests.get(repo_url)
+    try:
+        return response.json()
+    except:
+        print('Invalid json: ' + repo_url)
+        return None
+
+
+def get_repo_endpoint_from_public_url(public_repo_url):
+    # https://github.com/DemocracyLab/CivicTechExchange
+    path_parts = urlparse.urlparse(public_repo_url)
+    pprint(path_parts)
+    if hasattr(path_parts, 'path'):
+        url_parts = path_parts.path.split('/')
+        if len(url_parts) > 1:
+            return 'https://api.github.com/repos/{owner}/{repo}/commits'.format(owner=url_parts[1], repo=url_parts[2])
+
+
+def get_latest_commit_date(repo_info):
+    return repo_info[0]["commit"]['author']['date']
--- common/management/commands/project_external_updates.py
+++ common/management/commands/project_external_updates.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
-from common.helpers.github import fetch_github_repository_info
+from common.helpers.github import fetch_github_repository_info, get_latest_commit_date, get_repo_endpoint_from_public_url
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -9,15 +10,23 @@ class Command(BaseCommand):
 
 
 def get_project_github_links():
-    # Retrieve all of the ProjectLink objects that are for Github Links
-    pass
+    from civictechprojects.models import ProjectLink
+    # TODO: Filter out non-github links
+    return ProjectLink.objects.filter(link_name='link_coderepo')
 
 
-def handle_project_github_updates(github_link):
+def handle_project_github_updates(project_github_link):
     # Get last updated time
-    repo = fetch_github_repository_info(github_link.link_url)
-    # If time more recent than project current updated time, update project's update time
-    pass
+    print(project_github_link.link_url)
+    repo_url = get_repo_endpoint_from_public_url(project_github_link.link_url)
+    if repo_url is not None:
+        repo_info = fetch_github_repository_info(repo_url)
+        if repo_info is not None:
+            print(repo_info)
+            latest_commit_date = get_latest_commit_date(repo_info)
+            print(latest_commit_date)
+            # If time more recent than project current updated time, update project's update time
+
 
 
 
