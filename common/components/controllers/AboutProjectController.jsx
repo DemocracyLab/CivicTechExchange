0213eb5c623345ff3fc7be44bebc00149d8acb64
Marlon Keating <marlonakeating@gmail.com>
2019-12-03 21:25:51 -0800

    Return prerender 404 header for projects that have been deleted

--- common/components/controllers/AboutProjectController.jsx
+++ common/components/controllers/AboutProjectController.jsx
@@ -1,6 +1,7 @@
 // @flow
 
 import React from 'react';
+import Helmet from 'react-helmet';
 import ProjectAPIUtils from '../utils/ProjectAPIUtils.js';
 import type {ProjectDetailsAPIData} from '../utils/ProjectAPIUtils.js';
 import metrics from "../utils/metrics.js";
@@ -13,7 +14,8 @@ import url from "../utils/url.js";
 
 type State = {|
   project: ?ProjectDetailsAPIData,
-  loadStatusMsg: stringA
+  loadStatusMsg: string,
+  statusCode: string
 |};
 
 class AboutProjectController extends React.PureComponent<{||}, State> {
@@ -40,24 +42,25 @@ class AboutProjectController extends React.PureComponent<{||}, State> {
 
   handleLoadProjectFailure(error: APIError) {
     this.setState({
-      loadStatusMsg: "Could not load project"
+      loadStatusMsg: "Could not load project",
+      statusCode: "404"
     });
   }
 
   render(): $React$Node {
-    return this.state.project ? this._renderDetails() : <div>{this.state.loadStatusMsg}</div>
+    return this.state.project ? this._renderDetails() : this._renderLoadMessage();
   }
 
   _renderDetails(): React$Node {
     return (
       <React.Fragment>
-        {this._renderHeader(this.state.project)}
+        {this._renderProjectHeader(this.state.project)}
         <AboutProjectDisplay project={this.state.project} viewOnly={false}/>
       </React.Fragment>
     );
   }
 
-  _renderHeader(project: ProjectDetailsAPIData): React$Node {
+  _renderProjectHeader(project: ProjectDetailsAPIData): React$Node {
     const title: string = project.project_name + " | DemocracyLab";
     const description: string = project.project_short_description || Truncate.stringT(project.project_description, 300);
 
@@ -67,6 +70,25 @@ class AboutProjectController extends React.PureComponent<{||}, State> {
         description={description}
         thumbnailUrl={project.project_thumbnail && project.project_thumbnail.publicUrl}
       />
+    );
+  }
+  
+  _renderLoadMessage(): React$Node {
+    return (
+      <React.Fragment>
+        {this._renderStatusCodeHeader()}
+        <div>{this.state.loadStatusMsg}</div>
+      </React.Fragment>
+    );
+  }
+  
+  _renderStatusCodeHeader(): React$Node {
+    return this.state.statusCode && (
+      <React.Fragment>
+        <Helmet>
+          <meta name="prerender-status-code" content={this.state.statusCode} />
+        </Helmet>
+      </React.Fragment>
     );
   }
 }
