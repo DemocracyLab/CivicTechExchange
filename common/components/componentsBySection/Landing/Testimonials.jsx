ee02e3a003c8e2e6d5f889e3f2bd214742383d17
Peter Breen <pbreen@gmail.com>
2020-01-21 12:49:28 -0800

    add project leader testimonials

---
+++ common/components/componentsBySection/Landing/Testimonials.jsx
@@ -0,0 +1,80 @@
+const Testimonials = [
+  {
+    name: "Maxx Silver",
+    avatar: "",
+    role: "",
+    project: "FareStart",
+    text: "DemocracyLab has been a great resource for FareStart as we’ve sought opportunities to connect with the local tech sector. Thanks to DemocracyLab, we’ve been able to recruit a diverse group of skilled volunteers. As a project leader, I’ve appreciated the structure DemocracyLab has provided, along with the best practices and reminders they’ve shared.  We are grateful for the platform and community that DemocracyLab has built.",
+    source: ""
+  },
+  {
+    name: "Joseph Peha",
+    avatar: "",
+    role: "",
+    project: "Ready Set Vote",
+    text: "DemocracyLab has been instrumental in adding momentum to Ready Set Vote. I can think of no better way to connect with volunteers, not only for hackathon-style events, but to cultivate ongoing, meaningful relationships with people who want to contribute to a project like ours.",
+    source: ""
+  },
+  {
+    name: "Will Saunders",
+    avatar: "",
+    role: "",
+    project: "Access and Connectivity Toolkit",
+    text: "DemocracyLab has become an essential networking tool and resource for state programs looking to engage the civic tech community.  The ability to credibly mobilize interested talent over longer periods has made a real difference to government stakeholders.",
+    source: ""
+  },
+  {
+    name: "Karen Perry",
+    avatar: "",
+    role: "",
+    project: "Access and Connectivity Toolkit",
+    text: "DemocracyLab has found the secret sauce to attract volunteer talent, inspirational projects, and put them together so that progress happens.  It may sound easy, but we know it’s not. DemocracyLab makes it work and it gets better with every hack.",
+    source: ""
+  },
+  {
+    name: "Scott Veirs",
+    avatar: "",
+    role: "",
+    project: "Orcasound",
+    text: "DemocracyLab has helped Orcasound build our open source community while making incremental progress on design and coding challenges during one day events. Many hackathon volunteers come and go, but some have dedicated hundreds of hours to our project outside of the hackathons themselves.",
+    source: ""
+  },
+  {
+    name: "Jackson Brown",
+    avatar: "",
+    role: "",
+    project: "Council Data Project",
+    text: "Our project has received incredible benefit from the DemocracyLab community. Whether it's development and design assistance or simply feedback and awareness for our project, DemocracyLab has been a valuable resource in furthering the goals of our work.",
+    source: ""
+  },
+  {
+    name: "Jenny Durkan",
+    avatar: "",
+    role: "Mayor, City of Seattle",
+    project: "",
+    text: "Seattle is home to some of the most innovative thinkers and technologists anywhere in the world who want to use their talent to address social challenges. It’s why we formed an Innovation Advisory Council, and it’s why groups like DemocracyLab exist.",
+    source: ""
+  },
+]
+
+/*
+{
+  name: "",
+  avatar: "",
+  role: "",
+  project: "",
+  text: "",
+  source: ""
+},
+*/
+
+/* Object Keys
+name: display name for person
+avatar: image URL for their photo, if we have it
+role: their role (Project Leader, volunteer...)
+project: project they most identify with / are identified with by us
+text:  testimonial text here
+source: for quotes about DL cited elsewhere, for attribution if required
+
+note that name and text are the only required fields; there are default values (avatar) or conditionals for the rest
+*/
