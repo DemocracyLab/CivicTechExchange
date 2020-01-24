33f4430a21a5c9b8a54d3035265d3d32271ddebc
Marlon Keating <marlonakeating@gmail.com>
2020-01-24 10:08:51 -0800

    SEO_JS_ENABLED should be set to False when anything other than 'True' is entered

--- democracylab/settings.py
+++ democracylab/settings.py
@@ -353,7 +353,7 @@ SEO_JS_PRERENDER_TOKEN = os.environ.get('SEO_JS_PRERENDER_TOKEN', '')
 SEO_JS_BACKEND = "common.helpers.caching.DebugPrerenderIO"
 SEO_JS_PRERENDER_URL = os.environ.get('SEO_JS_PRERENDER_URL', 'http://localhost:3000/')  # Note trailing slash.
 SEO_JS_PRERENDER_RECACHE_URL = SEO_JS_PRERENDER_URL + "recache"
-SEO_JS_ENABLED = os.environ.get('SEO_JS_ENABLED', False)
+SEO_JS_ENABLED = os.environ.get('SEO_JS_ENABLED', False) == 'True'
 
 # TODO: Put in environment variable
 SEO_JS_USER_AGENTS = (
