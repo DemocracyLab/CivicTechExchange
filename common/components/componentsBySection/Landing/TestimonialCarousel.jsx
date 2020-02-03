c62f00dcbcebc1ffff08b61acc921e48c069bc57
Peter Breen <pbreen@gmail.com>
2020-02-03 14:49:32 -0800

    add key to avoid React warning when mapping testimonials

--- common/components/componentsBySection/Landing/TestimonialCarousel.jsx
+++ common/components/componentsBySection/Landing/TestimonialCarousel.jsx
@@ -4,7 +4,7 @@ import Carousel from 'react-bootstrap/Carousel';
 import Person from '@material-ui/icons/Person';
 
 const carouselItems = Testimonials.map((i) =>
-  <Carousel.Item className="carousel-item">
+  <Carousel.Item className="carousel-item" key={i.name}>
     {i.avatar ? <img src={i.avatar} alt={i.name} className="carousel-avatar"></img> : null}
     <h3>{i.name}</h3>
     <h4>{i.role}, {i.project}</h4>
