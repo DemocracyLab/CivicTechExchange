a5e430b2c94790383334edf16e085402a2adbd0d
Marlon Keating <marlonakeating@gmail.com>
2020-03-04 12:45:39 -0800

    Sign Up: Don't attempt to subscribe user to mailing list if api key not set

--- common/helpers/mailing_list.py
+++ common/helpers/mailing_list.py
@@ -12,8 +12,16 @@ class SubscribeToMailingList(object):
         thread.daemon = True
         thread.start()
 
+    def print_error(self, err_msg):
+        err_msg = 'Failed to subscribe {first} {last}({email}) to mailing list: {err_msg}'.format(
+            first=self.first_name, last=self.last_name, email=self.email, err_msg=err_msg)
+        print(err_msg)
+
     def run(self):
-        # TODO: Don't attempt if API key not set
+        if settings.MAILCHIMP_API_KEY is None:
+            self.print_error('MAILCHIMP_API_KEY not set')
+            return False
+
         api_key = settings.MAILCHIMP_API_KEY
         list_id = settings.MAILCHIMP_SUBSCRIBE_LIST_ID
 
@@ -21,6 +29,6 @@ class SubscribeToMailingList(object):
         try:
             api.lists.subscribe(list_id, {'email': self.email})
         except (ListDoesNotExistError, EmailNotExistsError, ListAlreadySubscribedError, Error) as e:
-            print('Failed to subscribe user to mailing list: {first} {last} ({email}) due to {err}'
-                  .format(first=self.first_name, last=self.last_name, email=self.email, err=repr(e)))
+            self.print_error(repr(e))
             return False
+
