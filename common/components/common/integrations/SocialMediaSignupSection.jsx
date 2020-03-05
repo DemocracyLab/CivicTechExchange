84fa51201abc51460780deefb3f5d75a4bbff627
Marlon Keating <marlonakeating@gmail.com>
2020-03-05 00:56:47 -0800

    Use new svg icons and new svg loader for social logins

--- common/components/common/integrations/SocialMediaSignupSection.jsx
+++ common/components/common/integrations/SocialMediaSignupSection.jsx
@@ -1,17 +1,34 @@
 // @flow
 
 import React from 'react';
-import svg, {SVGPath} from "../../utils/svg";
 import CurrentUser from "../../utils/CurrentUser.js";
+import FacebookSVG from "../../svg/facebook.svg";
+import GithubSVG from "../../svg/github.svg";
+import GoogleSVG from "../../svg/google.svg";
+import LinkedInSVG from "../../svg/linkedin.svg";
 import _ from "lodash";
 
 const socialAppVisibility: { [key: string]: boolean } = !_.isEmpty(window.SOCIAL_APPS_VISIBILITY) ? JSON.parse(_.unescape(window.SOCIAL_APPS_VISIBILITY)) : {};
 
-const svgMap: { [key: string]: string } = {
-  "linkedin": "LINKEDIN",
-  "google": "GOOGLE",
-  "github": "GITHUB",
-  "facebook": "FACEBOOK"
+type SocialSignInLinkDisplayConfig = {|
+  displayName: string,
+  iconElement: React$Node
+|};
+
+const socialSignInLinkDisplayConfigMap: { [key: string]: SocialSignInLinkDisplayConfig } = {
+  linkedin: {
+    displayName: "LinkedIn",
+    iconElement: <LinkedInSVG />
+  }, google: {
+    displayName: "Google",
+    iconElement: <GoogleSVG />
+  }, github: {
+    displayName: "GitHub",
+    iconElement: <GithubSVG />
+  }, facebook: {
+    displayName: "Facebook",
+    iconElement: <FacebookSVG />
+  }
 };
 
 type State = {|
@@ -42,14 +59,16 @@ class SocialMediaSignupSection extends React.Component<{||}, State> {
   _renderSocialLogins(): React$Node {
     return (
       <div>
-        {this.state.visibleApps.map(app => {return (
+        {this.state.visibleApps.map(app => {
+          const config: SocialSignInLinkDisplayConfig = socialSignInLinkDisplayConfigMap[app];
+          return (
           <div className="LogInController-socialLink">
             <a href={"/login/" + app} key={app}>
-              <span>
-                {svg.path(SVGPath[svgMap[app]], "LogInController-socialIcon")}
+              <span style={{color: config.iconColor}} className="LogInController-socialIcon">
+                {config.iconElement}
               </span>
               <span>
-                Continue with {app}
+                Continue with {config.displayName}
               </span>
             </a>
           </div>
