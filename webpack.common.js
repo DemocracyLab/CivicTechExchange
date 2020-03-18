324ab55f500de57e06fbf7b7339a9c89285e5e06
Marlon Keating <marlonakeating@gmail.com>
2020-03-17 21:10:25 -0700

    Add css steps from deleted webpack.config.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const webpack = require('webpack');
 const ContextReplacementPlugin = require('webpack/lib/ContextReplacementPlugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
   entry: "./common/components/mount-components.js",
@@ -13,7 +15,24 @@ module.exports = {
     rules: [
         { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
         { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-        { test: /\.svg$/, use: ['@svgr/webpack'] }
+        { test: /\.svg$/, use: ['@svgr/webpack'] },
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 2
+              }
+            },
+            {
+              loader: 'sass-loader'
+            }
+          ]
+        }
     ]
   },
   resolve: {
@@ -29,7 +48,13 @@ module.exports = {
         'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
         'fs': 'empty'
     }),
-    new ContextReplacementPlugin(/moment[\/\\]locale$/, /en/)
+    new ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: 'css/styles.css',
+      allChunks: true,
+    })
   ],
   optimization: {
     runtimeChunk: 'single',
