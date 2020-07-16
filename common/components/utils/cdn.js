e8f7c1f98f52e4154404ecdf0b94598cfbcf28db
Peter Breen <pbreen@gmail.com>
2020-07-16 13:27:32 -0700

    change donate/thankyou images

--- common/components/utils/cdn.js
+++ common/components/utils/cdn.js
@@ -4,8 +4,8 @@ export const Images: { [key: string]: string } = {
   DL_GLYPH: "dl_identity_node_mark.png",
   PLATFORM_SPLASH: "platform_splash.png",
   EVENT_SPLASH: "event_splash.png",
-  THANK_YOU: "ThankYouBG_slim.jpg",
-  DONATE_SPLASH: "DonateBG.jpg",
+  THANK_YOU: "CodeForGood_072719_MSReactor-020.jpg",
+  DONATE_SPLASH: "SplashImage-7178-1400.jpg",
   PAYPAL_BUTTON: "PaypalDonateButton.png",
   CORE_VALUES_BG: "CoreValuesBG520.png",
   PROBLEM_SOLUTION_BG: "PuzzleBG.png"
