608ddd4fbb61177dcd97e5d32839bc18b0e9050d
Asher Dale <t-asdale@microsoft.com>
2020-07-28 17:14:10 -0400

    Add search and filter capabilities for entities on admin site

--- common/admin.py
+++ common/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Tag
 
-admin.site.register(Tag)
+tag_text_fields = ['tag_name', 'display_name', 'caption', 'category', 'subcategory', 'parent']
+class TagAdmin(admin.ModelAdmin):
+    list_display = tuple(tag_text_fields)
+    search_fields = tag_text_fields
+    list_filter = ('category', 'subcategory')
+
+admin.site.register(Tag, TagAdmin)
--- democracylab/admin.py
+++ democracylab/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 
 from .models import Contributor
 
-admin.site.register(Contributor)
+contributor_text_fields = ['email', 'first_name', 'last_name', 'about_me', 'country', 'postal_code', 'phone_primary', 'about_me', 'uuid', 'qiqo_uuid']
+contributor_filter_fields = ('email_verified',)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = tuple(contributor_text_fields) + contributor_filter_fields
+    search_fields = contributor_text_fields
+    list_filter = contributor_filter_fields
+
+admin.site.register(Contributor, ContributorAdmin)
