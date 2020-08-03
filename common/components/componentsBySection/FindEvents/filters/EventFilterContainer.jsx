18bb3173bf30464b068301eff35469348632ba9a
Peter Breen <pbreen@gmail.com>
2020-08-03 09:58:48 -0700

    add skeleton files for Find Events

---
+++ common/components/componentsBySection/FindEvents/filters/EventFilterContainer.jsx
@@ -0,0 +1,22 @@
+
+// @flow
+//
+// import React from 'react';
+// import GroupFilterDataContainer from "./GroupFilterDataContainer.jsx";
+// import ResetGroupSearchButton from "./ResetGroupSearchButton.jsx";
+//
+// class GroupFilterContainer extends React.PureComponent<{||}> {
+//
+//   render(): React$Node {
+//     return (
+//       <div className="ProjectFilterContainer-root col-12 col-md-4 col-lg-3 pl-0 pr-0">
+//         <div className="ProjectFilterContainer-reset">
+//           <ResetGroupSearchButton />
+//         </div>
+//           <GroupFilterDataContainer />
+//       </div>
+//     );
+//   }
+// }
+//
+// export default GroupFilterContainer;
---
+++ common/components/controllers/FindEventsController.jsx
@@ -0,0 +1,36 @@
+// @flow
+
+import Headers from "../common/Headers.jsx";
+import SplashScreen, {HeroImage} from "../componentsBySection/FindProjects/SplashScreen.jsx";
+import EventCardsContainer from "../componentsBySection/FindEvents/EventCardsContainer.jsx";
+import EventFilterContainer from "../componentsBySection/FindEvents/filters/EventFilterContainer.jsx";
+import React from 'react';
+
+class FindEventsController extends React.PureComponent {
+  constructor(): void {
+    super();
+  }
+
+  render(): React$Node {
+    return (
+      <React.Fragment>
+        <Headers
+          title="DemocracyLab"
+          description="Optimizing the connection between skilled volunteers and tech-for-good events"
+        />
+        <div className="FindEventsController-root">
+          <SplashScreen className="FindEventsController-topsplash" header={"Find Events Title Goes Here"} img={HeroImage.FindEvents}>
+          <div className="container">
+            <div className="row">
+              <EventFilterContainer />
+              <EventCardsContainer showSearchControls={true}/>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default FindGroupsController;
