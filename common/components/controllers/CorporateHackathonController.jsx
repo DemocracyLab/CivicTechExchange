964676a75b11720702345f4cfe63b87631a500b7
Peter Breen <pbreen@gmail.com>
2020-08-17 20:08:32 -0700

    add svg and one missing text block, begin mobile styling

--- common/components/controllers/CorporateHackathonController.jsx
+++ common/components/controllers/CorporateHackathonController.jsx
@@ -4,7 +4,15 @@ import React from 'react';
 import Headers from "../common/Headers.jsx";
 import ContactForm from "../forms/ContactForm.jsx";
 import prerender from "../utils/prerender.js";
+import url from "../utils/url.js";
 import Button from "react-bootstrap/Button";
+import Section from "../enums/Section";
+import Ch1Icon from "../svg/corporatehackathon/one.svg";
+import Ch2Icon from "../svg/corporatehackathon/two.svg";
+import Ch3Icon from "../svg/corporatehackathon/three.svg";
+import cdn from "../utils/cdn.js";
+
+
 
 class CorporateHackathonController extends React.PureComponent<{||}> {
   componentDidMount() {
@@ -45,6 +53,25 @@ class CorporateHackathonController extends React.PureComponent<{||}> {
         <p>Create a unique employee engagement experience and deliver instant-impact tech projects.</p>
         <Button variant="primary">Get Started</Button>
         <div><p> img goes here</p></div>
+        <h2>Public Hackathon Results</h2>
+        <div className="corporate-hackathon-hostwithus-results">
+          <div className="corporate-hackathon-hostwithus-results-item">
+            <h3>$1 million +</h3>
+            <p>Public Value Created</p>
+          </div>
+          <div className="corporate-hackathon-hostwithus-results-item">
+            <h3>1200</h3>
+            <p>Volunteers</p>
+          </div>
+          <div className="corporate-hackathon-hostwithus-results-item">
+            <h3>100 +</h3>
+            <p>Projects</p>
+          </div>
+          <div className="corporate-hackathon-hostwithus-results-item">
+            <h3>100</h3>
+            <p>Average Attendees/Event</p>
+          </div>
+        </div>
       </div>
     )
   }
@@ -54,15 +81,18 @@ class CorporateHackathonController extends React.PureComponent<{||}> {
         <h2>How it works</h2>
         <p>As soon as you have signed up for a hackathon, the DemocracyLab team gets right to work on finding non-profits in need of tech volunteers. You can focus on team building; we’ll get everything set up with your specific goals in mind. There are just a few things we need from you!</p>
         <div className="corporate-hackathon-howitworks-one">
-          <p>1. Recruit (svg here)</p>
+          <img src={cdn.image("recruit-icon.png")} alt="Recruit"/>
+          <p><Ch1Icon /> Recruit</p>
           <p>Encourage your employees to sign up for the hackathon! We’ll find non-profits with needs that are a good fit for your team.</p>
         </div>
         <div className="corporate-hackathon-howitworks-two">
-          <p>2. Define (svg here)</p>
+          <img src={cdn.image("define-icon.png")} alt="Define"/>
+          <p><Ch2Icon /> Define</p>
           <p>We’ll work with the non-profits to identify achievable, instant-impact projects that are ready for your team, then connect you with the non-profit team to define the scope.</p>
         </div>
         <div className="corporate-hackathon-howitworks-three">
-          <p>3. Hack (svg here)</p>
+          <img src={cdn.image("hack-icon.png")} alt="Hack"/>
+          <p><Ch3Icon /> Hack</p>
           <p>We’ll work with the non-profits to identify achievable, instant-impact projects that are ready for your team, then connect you with the non-profit team to define the scope.</p>
         </div>
         <p>Once the hackathon is finished, you’re all done! Your team can get back to work and enjoy renewed enthusiasm and deeper engagement. DemocracyLab will survey both volunteers and project owners to provide you a clear summary of the outcomes and impacts of the event.</p>
@@ -111,6 +141,7 @@ class CorporateHackathonController extends React.PureComponent<{||}> {
           <h3>Event Sponsorship</h3>
           <h4>Starting at $500 per event</h4>
           <p>Not ready to host? Sponsor one of our public hackathons! Your sponsorship will help us drive innovation, support non-profits and civic tech organizations, and promote your brand before, during, and after the event!</p>
+          <Button variant="primary" href={url.section(Section.PartnerWithUs)}>Learn More</Button>
         </div>
       </div>
     )
