c2ac5d26402d163b93bfc7f5a1ed3bfd53e5cbb9
Marlon Keating <marlonakeating@gmail.com>
2020-09-09 20:02:50 -0700

    Stop caching project tags call

--- civictechprojects/views.py
+++ civictechprojects/views.py
@@ -39,9 +39,6 @@ from django.views.decorators.cache import cache_page
 import requests
 
 
-
-# TODO: Set getCounts to default to false if it's not passed? Or some hardening against malformed API requests
-@cache_page(1200) #cache duration in seconds, cache_page docs: https://docs.djangoproject.com/en/2.1/topics/cache/#the-per-view-cache
 def tags(request):
     url_parts = request.GET.urlencode()
     query_terms = urlparse.parse_qs(
