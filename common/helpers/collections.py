ec85040cd479578857c4965836d88667700b3c2f
Marlon Keating <marlonakeating@gmail.com>
2020-09-17 10:08:22 -0700

    Re-cache projects when volunteer profiles change

--- common/helpers/collections.py
+++ common/helpers/collections.py
@@ -22,3 +22,16 @@ def count_occurrences(collection):
         else:
             count_dict[item] = count_dict[item] + 1
     return count_dict
+
+
+def distinct(list_a, list_b, key_func):
+    """
+    Take two lists of arbitrary objects and get the union of distinct objects between the two
+    :param list_a: First object list
+    :param list_b: Second object list
+    :param key_func: function for extracting unique key from objects
+    :return:
+    """
+    lists_dict = {key_func(item): item for item in list_a}
+    lists_dict.update({key_func(item): item for item in list_b})
+    return lists_dict.values()
