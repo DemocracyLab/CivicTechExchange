d2742eb3545b059c466047b0f2354e041c47ac93
Marlon Keating <marlonakeating@gmail.com>
2020-09-24 23:27:31 -0700

    Event Profile: Show different view for multi-day event

--- common/components/utils/datetime.js
+++ common/components/utils/datetime.js
@@ -5,6 +5,7 @@ import moment from "moment-timezone";
 
 export const DateFormat: {[key: string]: string} = {
   DAY_MONTH_DATE_YEAR: "dddd, MMMM Do YYYY",
+  DAY_MONTH_DATE_YEAR_TIME: "dddd, MMMM Do YYYY h:mm a z",
   MONTH_DATE_YEAR: "MMMM Do YYYY",
   TIME: "h:mm a",
   TIME_TIMEZONE: "h:mm a z",
@@ -15,6 +16,10 @@ export const timezone: string = moment.tz.guess();
 class datetimeHelper {
   static parse(dateString: string): Moment {
     return moment(dateString).tz(timezone);
+  }
+  
+  static isOnSame(period: string, a: Moment, b: Moment): boolean {
+    return a.clone().startOf(period).isSame(b.clone().startOf(period));
   }
 }
 
