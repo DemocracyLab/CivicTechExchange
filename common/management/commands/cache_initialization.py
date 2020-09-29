b05f383637e0cbe8383e9f1956834db3ad355855
Marlon Keating <marlonakeating@gmail.com>
2020-09-28 19:21:53 -0700

    cache_initialization job should always re-cache projects

--- common/management/commands/cache_initialization.py
+++ common/management/commands/cache_initialization.py
@@ -6,12 +6,7 @@ from democracylab.emails import send_volunteer_application_email
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        from civictechprojects.caching.cache import ProjectCache
         from civictechprojects.models import Project
         projects_to_cache = Project.objects.filter(is_searchable=True, deleted=False)
         for project in projects_to_cache:
-            cached_project = ProjectCache.get(project)
-            if cached_project is None:
-                print('Cached project not found, re-caching: ' + project.__str__())
-                project.recache()
-
+            project.recache()
