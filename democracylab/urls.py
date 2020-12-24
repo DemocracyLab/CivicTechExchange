c04678ac079c8abab4b68fdbf07b8f55ad263193
marlonkeating <marlonakeating@gmail.com>
2020-12-23 23:28:23 -0800

    Django 3 Upgrade: Update auth_views logout view reference

--- democracylab/urls.py
+++ democracylab/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', views.login_view, name='login_view'),
     url(r'^login/(?P<provider>\w+)', views.login_view, name='login_view'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
     url(
         r'^password_reset/$',
         views.password_reset,
