fa03330b5f6e2d682463869dec2d8e6edd80be5d
Peter Breen <pbreen@gmail.com>
2021-01-11 11:41:06 -0800

    formatting cleanup

--- common/components/controllers/DonateController.jsx
+++ common/components/controllers/DonateController.jsx
@@ -1,34 +1,34 @@
 // @flow
 
 import React from 'react';
-import cdn,{Images} from "../utils/cdn.js";
+import cdn, { Images } from "../utils/cdn.js";
 import RadioButtons from "../common/selection/RadioButtons.jsx";
-import PaypalDonationButton, {OtherAmountSelected} from "../common/integrations/PaypalDonationButton.jsx";
-import {SelectOption} from "../types/SelectOption.jsx";
+import PaypalDonationButton, { OtherAmountSelected } from "../common/integrations/PaypalDonationButton.jsx";
+import { SelectOption } from "../types/SelectOption.jsx";
 import DonateBlurb from "../componentsBySection/Donate/DonateBlurb.jsx";
 
 type State = {|
   donateAmount: ?string,
-  donateMonthly: ?string
-|};
+    donateMonthly: ?string
+      |};
 
 const DonationAmountOptions: $ReadOnlyArray<SelectOption> = [
-  {label: "$10", value: "10"},
-  {label: "$25", value: "25"},
-  {label: "$50", value: "50"},
-  {label: "$75", value: "75"},
-  {label: "$100", value: "100"},
-  {label: "$250", value: "250"},
-  {label: "$500", value: "500"},
-  {label: "Other", value: OtherAmountSelected}
+  { label: "$10", value: "10" },
+  { label: "$25", value: "25" },
+  { label: "$50", value: "50" },
+  { label: "$75", value: "75" },
+  { label: "$100", value: "100" },
+  { label: "$250", value: "250" },
+  { label: "$500", value: "500" },
+  { label: "Other", value: OtherAmountSelected }
 ];
 
 const DonationMonthlyOptions: $ReadOnlyArray<SelectOption> = [
-  {label: "One-Time", value: false},
-  {label: "Monthly", value: true}
+  { label: "One-Time", value: false },
+  { label: "Monthly", value: true }
 ];
 
-class DonateController extends React.Component<{||}, State> {
+class DonateController extends React.Component<{||}, State > {
   constructor(): void {
     super();
     this.state = {
@@ -58,7 +58,7 @@ class DonateController extends React.Component<{||}, State> {
               <p>
                 DemocracyLab is building online infrastructure to support the technology-for-good movement. Your donation will help us support the many projects and volunteers who use our platform to make our world a better place. DemocracyLab is a 501(c)(3) nonprofit organization, and your donation may be tax-deductible.
               </p>
-              <DonateBlurb/>
+              <DonateBlurb />
 
               <div className="DonateController-options">
                 <div className="DonateController-amounts">
@@ -70,20 +70,20 @@ class DonateController extends React.Component<{||}, State> {
                 </div>
 
                 {this.state.donateAmount !== OtherAmountSelected
-                ? < div className="DonateController-monthly">
-                <h3 className="DonateController-subheader">Frequency</h3>
-                <RadioButtons
-                  variant="outline-dark"
-                  options={DonationMonthlyOptions}
-                  defaultSelection={DonationMonthlyOptions[0]}
-                  onSelection={this.handleFieldSelection.bind(this, "donateMonthly")}
-                />
-                </div>
-                : null
+                  ? < div className="DonateController-monthly">
+                    <h3 className="DonateController-subheader">Frequency</h3>
+                    <RadioButtons
+                      variant="outline-dark"
+                      options={DonationMonthlyOptions}
+                      defaultSelection={DonationMonthlyOptions[0]}
+                      onSelection={this.handleFieldSelection.bind(this, "donateMonthly")}
+                    />
+                  </div>
+                  : null
                 }
                 <PaypalDonationButton
-                donateAmount={this.state.donateAmount}
-                donateMonthly={this.state.donateMonthly}
+                  donateAmount={this.state.donateAmount}
+                  donateMonthly={this.state.donateMonthly}
                 />
               </div>
             </div>
