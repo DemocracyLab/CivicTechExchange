cc27a0ef3f80dd557349a86b8be6d8dce47fdb9a
Peter Breen <pbreen@gmail.com>
2021-05-10 14:38:04 -0700

    initial setup for changes
    Clarify purpose of bootstrapoverrides vs global
    remove temporary font rules from Corporate Hackathon and Profile pages
    add design system fonts to Global
    update styles to indicate Global's new significance
    
    in effect, this sets the foundation but every page will require some significant modification yet

--- common/components/controllers/CorporateHackathonController.jsx
+++ common/components/controllers/CorporateHackathonController.jsx
@@ -271,13 +271,13 @@ class CorporateHackathonController extends React.PureComponent<{||}, State> {
           <div className="carousel-blog-root">
             <BlogCarousel tag="partner-highlights" interval={600000} />
           </div>
-          <p className="size-h2">
+          <p className="h2">
             Read about other previous events in our{" "}
-            <a className="size-h2" href={window.BLOG_URL} target="_blank">
+            <a className="h2" href={window.BLOG_URL} target="_blank">
               blog
             </a>
             , or other events that are coming up{" "}
-            <a className="size-h2" href={url.section(Section.FindEvents)}>
+            <a className="h2" href={url.section(Section.FindEvents)}>
               here
             </a>
             .
@@ -411,13 +411,13 @@ class CorporateHackathonController extends React.PureComponent<{||}, State> {
           <div className="carousel-blog-root">
             <BlogCarousel tag="hackathon-highlights" interval={600000} />
           </div>
-          <p className="size-h2">
+          <p className="h2">
             Read about other previous events in our{" "}
-            <a className="size-h2" href={window.BLOG_URL} target="_blank">
+            <a className="h2" href={window.BLOG_URL} target="_blank">
               blog
             </a>
             , or other events that are coming up{" "}
-            <a className="size-h2" href="/events">
+            <a className="h2" href="/events">
               here
             </a>
             .
