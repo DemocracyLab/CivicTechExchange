9ff5f49839f255a6d702e2b0a6d11a0ff925bdcf
marlonkeating <marlonakeating@gmail.com>
2021-07-15 09:47:47 -0700

    Video page: Add css for responsive video element

--- common/components/controllers/VideoController.jsx
+++ common/components/controllers/VideoController.jsx
@@ -10,15 +10,17 @@ class VideoController extends React.PureComponent {
 
   render(): React$Node {
     return (
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/nvIUWtx-nmo"
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <div className="youtube-video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/nvIUWtx-nmo"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
     );
   }
 }
