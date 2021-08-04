25161ac5bf4789cefb1c60db9795e4ceccbe18dc
marlonkeating <marlonakeating@gmail.com>
2021-08-04 13:07:04 -0700

    MaliciousRequestsMiddle: Read malicious url patterns from MALICIOUS_URL_PATTERNS

--- common/helpers/malicious_requests.py
+++ common/helpers/malicious_requests.py
@@ -1,3 +1,6 @@
+import re
+from django.conf import settings
+from django.core.exceptions import MiddlewareNotUsed
 from django.http import HttpResponseBadRequest
 
 
@@ -5,16 +8,21 @@ class MaliciousRequestsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         # One-time configuration and initialization.
+        if settings.MALICIOUS_URL_PATTERNS is not None:
+            patterns = settings.MALICIOUS_URL_PATTERNS.split(',')
+            self.malicious_url_patterns = list(map(lambda pattern: re.compile(pattern, re.IGNORECASE), patterns))
+        else:
+            raise MiddlewareNotUsed
 
     def __call__(self, request):
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
         path = request.get_full_path()
-        # TODO: Put in separate module
-        # TODO: Read from regex environment variable
-        if 'escaped_fragment' in path:
-            return HttpResponseBadRequest
+        for pattern in self.malicious_url_patterns:
+            if pattern.search(path) is not None:
+                # TODO: Log
+                return HttpResponseBadRequest
 
         response = self.get_response(request)
 
