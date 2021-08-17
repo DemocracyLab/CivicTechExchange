c3afd749c131cceaf49055e9542e36342bc9759d
marlonkeating <marlonakeating@gmail.com>
2021-08-17 11:58:03 -0700

    getSortedCategoryTags: Only include tags with num_times > 0

--- common/components/utils/tags.js
+++ common/components/utils/tags.js
@@ -1,22 +1,24 @@
 // @flow
 
-import type { TagDefinition } from "./ProjectAPIUtils.js";
-import type { Dictionary, PartitionSet } from "../types/Generics.jsx";
+import type { TagDefinitionCount } from "./ProjectAPIUtils.js";
+import type { Dictionary } from "../types/Generics.jsx";
 import _ from "lodash";
 
 export type TagsByCategory = Dictionary<
-  $ReadOnlyArray<TagDefinition> | Dictionary<$ReadOnlyArray<TagDefinition>>
+  | $ReadOnlyArray<TagDefinitionCount>
+  | Dictionary<$ReadOnlyArray<TagDefinitionCount>>
 >;
 
 class tagHelpers {
   // Get sorted tags grouped by category
   // TODO: Unit test
   static getSortedTagsByCategory(
-    tags: $ReadOnlyArray<TagDefinition>
+    tags: $ReadOnlyArray<TagDefinitionCount>
   ): TagsByCategory {
-    const sortedTags: $ReadOnlyArray<TagDefinition> = _.sortBy(tags, [
-      "display_name",
-    ]);
+    const sortedTags: $ReadOnlyArray<TagDefinitionCount> = _.chain(tags)
+      .filter((tag: TagDefinitionCount) => tag.num_times > 0)
+      .sortBy(["display_name"])
+      .value();
     let tagsByCategory: TagsByCategory = _.groupBy(sortedTags, "category");
     _.keys(tagsByCategory).forEach((category: string) => {
       if (tagsByCategory[category][0].subcategory) {
