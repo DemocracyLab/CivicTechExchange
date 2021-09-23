8303efe4be5cf9e4c128338524ec0de90e3f4fc4
Peter Breen <pbreen@gmail.com>
2021-09-23 13:45:52 -0700

    prototype of modal repurposed as offCanvas

--- common/components/componentsBySection/FindProjects/Filters/ProjectFilterContainer.jsx
+++ common/components/componentsBySection/FindProjects/Filters/ProjectFilterContainer.jsx
@@ -17,6 +17,9 @@ import _ from "lodash";
 import { List } from "immutable";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import ModalWrapper from "../../../common/ModalWrapper.jsx";
 
 /**
  * @category: Tag category to pull from
@@ -29,8 +32,10 @@ type Props = {|
 type State = {|
   tags: ?$ReadOnlyArray<TagDefinition>,
   selectedTags: ?{ [key: string]: boolean },
+  showOffCanvasFilters: boolean,
 |};
 
+//TODO: isReady does not work - need to show LoadingFrame or null until we get filter data from flux
 class ProjectFilterContainer extends React.Component<Props, State> {
   constructor(props: Props): void {
     super(props);
@@ -43,6 +48,7 @@ class ProjectFilterContainer extends React.Component<Props, State> {
       orgType: {},
       selectedTags: {},
       isReady: false,
+      showOffCanvasFilters: false,
     };
     this._selectOption = this._selectOption.bind(this);
     this._checkEnabled = this._checkEnabled.bind(this);
@@ -76,48 +82,33 @@ class ProjectFilterContainer extends React.Component<Props, State> {
   }
 
   render(): React$Node {
-    //should render a number of Flux view child components
-    // TODO: verify if we need to keep this as a nav
-    // TODO: Test instead of render null before ready, render a LoadingFrame?
-    return (
-      <Navbar
-        expand="lg"
-        bg="navlight"
-        variant="light"
-        className="ProjectFilterContainer-root nav-pills pills-outline pills-rounded"
-      >
-        <Navbar.Toggle aria-controls="ProjectFilterContainer-root" />
-        <Navbar.Collapse
-          id="ProjectFilterContainer-root"
-          className="flex-column"
+    return this.state.isReady ? (
+      <React.Fragment>
+        <Navbar
+          expand="lg"
+          bg="navlight"
+          variant="light"
+          className="ProjectFilterContainer-root"
         >
+          <Navbar.Toggle aria-controls="ProjectFilterContainer-root" />
+          <Navbar.Collapse
+            id="ProjectFilterContainer-root"
+            className="flex-column"
+          >
             <Nav className="ProjectFilterContainer-nav mr-auto">
-            {this.state.isReady ? this._displayFilters() : null}
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
+              {this._displayFilters()}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        {this._showOffCanvasFilters()}
+      </React.Fragment>
+    ) : null;
   }
 
   _displayFilters(): React$Node {
-    // console.log("TESTING getSortedCategoryTags below:");
-    // console.log(ProjectSearchStore.getSortedCategoryTags("Role").toArray());
-    // console.log(
-    //   ProjectSearchStore.getSortedCategoryTags("Technologies Used").toArray()
-    // );
-    // console.log(
-    //   ProjectSearchStore.getSortedCategoryTags("Project Stage").toArray()
-    // );
-    // console.log(
-    //   ProjectSearchStore.getSortedCategoryTags("Organization Type").toArray()
-    // );
-
-    //TODO: mobile (OffCanvas possibly?), RFC needs subcat, filter functionality
-    //possible ref: https://bootstrap-menu.com/detail-offcanvas-mobile.html
     // this is more verbose than it 'needs to be' and we may reduce this to a map later, but it's code readability vs length
-    //TODO: IsReady prop isn't working as I want -- these render empty and then refresh full rather than waiting for ready
     return (
-      this.state.isReady ? (<React.Fragment>
+      <React.Fragment>
         <RenderFilterCategory
           cdata={this.state.rolesNeeded}
           displayName={"Roles Needed"}
@@ -154,8 +145,43 @@ class ProjectFilterContainer extends React.Component<Props, State> {
           checkEnabled={this._checkEnabled}
           selectOption={this._selectOption}
         />
-      </React.Fragment>) : null
+      </React.Fragment>
     );
+  }
+
+  //TODO: calling displayFilters here alone may not be correct (calls dropdown items without parent Nav wrapper)
+  // also refactor to avoid calling twice if possible
+  _showOffCanvasFilters(): React$Node {
+    return (
+      <React.Fragment>
+        <Button variant="primary" onClick={() => this._handleModalClick()}>
+          Filter Results
+        </Button>
+          <Modal
+            className="ProjectFilterContainer-offcanvas fixed-left"
+            show={this.state.showOffCanvasFilters}
+            onHide={() => this._handleModalClick()}
+            aria-labelledby="ProjectFilterContainer-offcanvas-title"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="ProjectFilterContainer-offcanvas-title">
+                Filter Results
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{this._displayFilters()}</Modal.Body>
+            <Modal.Footer>
+              <Button variant="outline-primary">Button</Button>
+              <Button variant="outline-secondary">Button #2</Button>
+            </Modal.Footer>
+          </Modal>
+      </React.Fragment>
+    );
+  }
+
+  _handleModalClick() {
+    this.setState(prevState => ({
+      showOffCanvasFilters: !prevState.showOffCanvasFilters,
+    }));
   }
 
   _checkEnabled(tag: TagDefinition): boolean {
