0396d0dba643bbb043ddc3ef8773a6a7a4d914e4
D-Bits <lockwood.da88@gmail.com>
2021-10-06 16:40:30 -0700

    Completed unit test for distinct()

--- common/tests/test_collections.py
+++ common/tests/test_collections.py
@@ -12,7 +12,9 @@ class CollectionsTests(TestCase):
         self.test_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
         # For testing the count_occurrences() and find_first() methods
         self.duplicates_list = [1, 2, 4, 4, 6, 5, 2, 8, 1, 6]
-        
+        # For distinct() function
+        self.distinct_list = [2, 8, 9, 4, 12, 15]
+        self.dict_values = {}
 
     # Test how many times different values occur
     def test_count_occurrences(self):
@@ -41,3 +43,14 @@ class CollectionsTests(TestCase):
         )
         self.assertEqual(func_call, 6)
     
+    def test_distinct(self):
+
+        func_call = distinct(
+            self.duplicates_list,
+            self.distinct_list,
+            lambda x:x 
+        )
+        expected_vals = [1, 2, 4, 6, 5, 8, 9, 12, 15]
+
+        self.assertEqual(list(func_call), expected_vals)
+
