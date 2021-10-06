ea9dac38c70ace57b42e06323ba788c4ebe4cbe7
D-Bits <lockwood.da88@gmail.com>
2021-10-06 12:52:39 -0700

    Got test for find_first working

--- common/tests/test_collections.py
+++ common/tests/test_collections.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from civictechprojects.models import VolunteerRelation
+from civictechprojects.models import VolunteerRelation, Project
 from common.helpers.collections import find_first, flatten, count_occurrences, distinct
+from democracylab.models import Contributor
 
 
 class CollectionsTests(TestCase):
@@ -8,9 +9,10 @@ class CollectionsTests(TestCase):
     def setUp(self):
 
         # For the flatten() function
-        self.test_array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        # For testing the count_occurrences() method
-        self.duplicates = [1, 2, 4, 4, 6, 5, 2, 8, 1, 6]
+        self.test_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        # For testing the count_occurrences() and find_first() methods
+        self.duplicates_list = [1, 2, 4, 4, 6, 5, 2, 8, 1, 6]
+        
 
     # Test how many times different values occur
     def test_count_occurrences(self):
@@ -23,10 +25,19 @@ class CollectionsTests(TestCase):
             5: 1,
             8: 1
         }
-        self.assertEqual(count_occurrences(self.duplicates), expected_results)
+        self.assertEqual(count_occurrences(self.duplicates_list), expected_results)
 
     def test_flatten(self):
 
-        data = self.test_array
+        data = self.test_list
         expected_results = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(expected_results, flatten(data))
+
+    def test_find_first(self):
+        
+        func_call = find_first(
+            self.duplicates_list,
+            lambda x: x > 4
+        )
+        self.assertEqual(func_call, 6)
+    
