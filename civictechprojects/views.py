e1dd73b0b7b408efba593588c7c4b1a73ca155bb
Zehua Zhang <42627607+zh-zhang916@users.noreply.github.com>
2021-11-24 15:42:36 -0800

    Update civictechprojects/views.py
    Co-authored-by: marlonkeating <322346+marlonkeating@users.noreply.github.com>

--- civictechprojects/views.py
+++ civictechprojects/views.py
@@ -1005,7 +1005,7 @@ def reject_project_volunteer(request, application_id):
         .paragraph('Hi {first_name},'.format(first_name=volunteer_relation.volunteer.first_name))\
         .paragraph('Thank you for your interest in volunteering with {project_name}.'.format(project_name=volunteer_relation.project.project_name))\
         .paragraph('Unfortunately, the project owner did not select you as a volunteer for this project.')\
-        .paragraph('Message from the project owner: \"\"{message}\"\"'.format(message=message))\
+        .paragraph('Message from the project owner: \"{message}\"'.format(message=message))\
         .paragraph('We hope you\'ll consider other volunteer opportunities at DemocracyLab.')\
         .button(url=find_projects_page_url, text='Explore More Projects')
         email_subject = 'Your volunteer application to join {project_name}'.format(
