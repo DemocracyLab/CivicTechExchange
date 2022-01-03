58b76125d4a5ce10e78a30a9f2f5e8d78a17db8e
marlonkeating <marlonakeating@gmail.com>
2022-01-03 14:51:47 -0800

    Salesforce Integration: Fix uploading project without tags selected

--- salesforce/campaign.py
+++ salesforce/campaign.py
@@ -12,30 +12,36 @@ def run(request):
 
 
 def save(project: object):
-    stage_tags = list(project.project_stage.all().values())
-    org_type_tags = list(project.project_organization_type.all().values())
-    tech_tags = list(project.project_technologies.all().values())
-    issue_area_tags = list(project.project_issue_area.all().values())
     data = {
-                "ownerid": client.owner_id,
-                "Project_Owner__r":
-                {
-                    "platform_id__c": project.project_creator.id
-                },
-                "recordtypeid": "01246000000uOeRAAU",
-                "name": project.project_name,
-                "type": ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in org_type_tags]),
-                "isactive": project.is_searchable,
-                "startdate": project.project_date_created.strftime('%Y-%m-%d'),
-                "issue_area__c": ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in issue_area_tags]),
-                "technologies__c": ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in tech_tags]),
-                "stage__c": ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in stage_tags]),
-                "project_url__c": project.project_url,
-                "description_action__c": project.project_description_actions,
-                "description_solution__c": project.project_description_solution,
-                "short_description__c": project.project_short_description,
-                "description": project.project_description
-            }
+        "ownerid": client.owner_id,
+        "Project_Owner__r":
+            {
+                "platform_id__c": project.project_creator.id
+            },
+        "recordtypeid": "01246000000uOeRAAU",
+        "name": project.project_name,
+        "isactive": project.is_searchable,
+        "startdate": project.project_date_created.strftime('%Y-%m-%d'),
+        "project_url__c": project.project_url,
+        "description_action__c": project.project_description_actions,
+        "description_solution__c": project.project_description_solution,
+        "short_description__c": project.project_short_description,
+        "description": project.project_description
+    }
+    # TODO: Handle null startdate
+
+    issue_area_tags = list(project.project_issue_area.all().values())
+    if issue_area_tags:
+        data['issue_area__c'] = ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in issue_area_tags])
+    stage_tags = list(project.project_stage.all().values())
+    if stage_tags:
+        data['stage__c'] = ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in stage_tags])
+    org_type_tags = list(project.project_organization_type.all().values())
+    if org_type_tags:
+        data['type'] = ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in org_type_tags])
+    tech_tags = list(project.project_technologies.all().values())
+    if tech_tags:
+        data['technologies__c'] = ",".join([Tag.get_by_name(tag.get('name')).display_name for tag in tech_tags])
     req = requests.Request(
         method="PATCH",
         url=f'{client.campaign_endpoint}/platform_id__c/{project.id}',
