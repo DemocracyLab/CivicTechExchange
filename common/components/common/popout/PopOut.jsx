fd9afaedd974cea6b56c2c4544bfe4b0540fe3bb
marlonkeating <marlonakeating@gmail.com>
2022-02-01 10:40:07 -0800

    Popper PopOut implementation

--- common/components/common/popout/PopOut.jsx
+++ common/components/common/popout/PopOut.jsx
@@ -1,6 +1,7 @@
 // @flow
 
-import React from "react";
+import React, { useState } from "react";
+import { usePopper } from "react-popper";
 import type { Dictionary } from "../../types/Generics.jsx";
 
 type Props = {|
@@ -10,61 +11,32 @@ type Props = {|
   onHide: () => void,
 |};
 
-type State = {|
-  popoverTop: number,
-  popoverLeft: number,
-|};
+const PopOut = (props: Props) => {
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: "auto-end",
+    strategy: "fixed",
+    modifiers: [{ name: "arrow", options: { element: arrowElement } }],
+  });
 
-class PopOut extends React.PureComponent<Props, State> {
-  constructor(props: Props): void {
-    super(props);
-    this.sourceRef = React.createRef();
-    this.frameRef = React.createRef();
-    this.state = this.generateFrameLocation();
-  }
+  return (
+    <React.Fragment>
+      <div ref={setReferenceElement}>{props.source}</div>
 
-  componentWillReceiveProps(nextProps: Props): void {
-    this.setState(this.generateFrameLocation(nextProps));
-  }
-
-  generateFrameLocation(props: Props): State {
-    let state: State = {};
-    if (props && props.source && this.sourceRef.getBoundingClientRect) {
-      const sourceRect: DOMRect = this.sourceRef.getBoundingClientRect();
-      state = {
-        popoverLeft: sourceRect.left,
-        popoverTop: sourceRect.bottom,
-      };
-    }
-    return state;
-  }
-
-  onClickOut(): void {
-    this.props.onHide();
-  }
-
-  render(): React$Node {
-    const style: Dictionary<string> = {
-      top: this.state.popoverTop,
-      left: this.state.popoverLeft,
-    };
-    if (!this.props.show) {
-      style.display = "none";
-    }
-    return (
-      <React.Fragment>
-        <div ref={this.sourceRef}>{this.props.source}</div>
+      {props.show && (
         <div
-          className="popout-clickout"
-          style={style}
-          onClick={this.onClickOut.bind(this)}
-        />
-        <div className="popout-frame" style={style} ref={this.frameRef}>
-          {this.props.frame}
+          ref={setPopperElement}
+          style={styles.popper}
+          {...attributes.popper}
+        >
+          <div className="popout-frame">{props.frame}</div>
+          <div ref={setArrowElement} style={styles.arrow} />
         </div>
-      </React.Fragment>
-    );
-  }
-}
+      )}
+    </React.Fragment>
+  );
+};
 
 export default PopOut;
