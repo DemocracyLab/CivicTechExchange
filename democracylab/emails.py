69fb352bc3c418c9daa2c4aa8b8039fefcb8b25e
Andrew I <86969595+AndrewIndeche@users.noreply.github.com>
2022-03-08 11:27:43 +0300

    Update democracylab/emails.py
    Co-authored-by: marlonkeating <322346+marlonkeating@users.noreply.github.com>

--- democracylab/emails.py
+++ democracylab/emails.py
@@ -420,7 +420,7 @@ def send_group_creation_notification(group):
 def notify_group_owners_group_approved(group):
     email_template = HtmlEmailTemplate() \
         .header('Congratulations! "{{group_name}}" has been approved.') \
-        .paragraph('Your group "{{group_name}}" has been approved. You can see it at {{group_url}}.') \
+        .paragraph('Your group "{{group_name}}" has been approved.') \
         .button(url='group_url', text='Go to Group Profile Page')
     context = {
         'group_name': group.group_name,
