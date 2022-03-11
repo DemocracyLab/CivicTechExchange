9bf6abb41634d5569bee931a75c5083484449330
marlonkeating <marlonakeating@gmail.com>
2022-03-11 12:57:14 -0800

    migrate_event_projects: Don't migrate already migrated projects

--- civictechprojects/migrations/data_migrations/migrate_event_projects.py
+++ civictechprojects/migrations/data_migrations/migrate_event_projects.py
@@ -9,10 +9,15 @@ def migrate_event_projects(*args):
         event_projects = event.get_linked_projects()
         if event_projects:
             for project in event_projects:
-                print('Migrating project: ' + project.__str__())
-                # Create Event Project from Project
-                event_project = EventProject.create(project.project_creator, event, project)
-                event_project.save()
-                # Remove event tag from Project
-                project.project_organization.remove(event.event_legacy_organization)
+                # Check to see if event project is already created
+                event_project = EventProject.get(event.id, project.id)
+                if not event_project:
+                    print('Migrating project: ' + project.__str__())
+                    # Create Event Project from Project
+                    event_project = EventProject.create(project.project_creator, event, project)
+                    event_project.save()
+                    # Remove event tag from Project
+                    project.project_organization.remove(event.event_legacy_organization)
+                else:
+                    print('{project} has already been migrated'.format(project=project.__str__()))
 
