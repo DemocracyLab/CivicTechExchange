2d1afddf49494db9b3ca4734dea9ec821196e5ef
marlonkeating <marlonakeating@gmail.com>
2022-03-18 14:49:26 -0700

    migrate_event_projects: Fix legacy project query

--- civictechprojects/migrations/data_migrations/migrate_event_projects.py
+++ civictechprojects/migrations/data_migrations/migrate_event_projects.py
@@ -1,4 +1,10 @@
-from civictechprojects.models import Event, EventProject
+from civictechprojects.models import Event, EventProject, Project
+
+
+def get_event_legacy_projects(event):
+    legacy_slugs = event.event_legacy_organization.slugs()
+    if legacy_slugs:
+        return Project.objects.filter(project_organization__name__in=legacy_slugs, is_searchable=True)
 
 
 def migrate_event_projects(*args):
@@ -6,8 +12,8 @@ def migrate_event_projects(*args):
     events = Event.objects.all()
     for event in events:
         print('Migrating projects for event: ' + event.__str__())
-        # Get list of projects for event
-        event_projects = event.get_linked_projects()
+        # Get list of legacy projects for event
+        event_projects = get_event_legacy_projects(event)
         if event_projects:
             for project in event_projects:
                 # Check to see if event project is already created
