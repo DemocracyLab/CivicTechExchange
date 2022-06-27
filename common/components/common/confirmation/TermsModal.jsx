e2f4e556f7f7f333bc4902db17102c36e96fd95a
Peter Breen <pbreen@gmail.com>
2022-06-27 11:04:16 -0700

    extract Terms text to own component, update TermsModal, populate Terms page

--- common/components/common/confirmation/TermsModal.jsx
+++ common/components/common/confirmation/TermsModal.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import NotificationModal from "../notification/NotificationModal.jsx";
 import type { Dictionary } from "../../types/Generics.jsx";
+import { TermsUse, TermsVolunteer } from "./TermsText.jsx"
 import _ from "lodash";
 
 export const TermsTypes: Dictionary<string> = {
@@ -80,136 +81,11 @@ class TermsModal extends React.PureComponent<Props, State> {
   }
 
   _renderUserSignupTerms(): React$Node {
-    return (
-      <ul>
-        <li>
-          I am volunteering solely for my personal purposes or pleasure,
-          including for civic, charitable, or humanitarian reasons.
-        </li>
-        <li>
-          I am freely donating my volunteer services and have no expectation of
-          receiving and have not been promised compensation or benefits in
-          exchange for any assistance I provide to project owners.
-        </li>
-        <li>
-          I am donating my volunteer services to the owner of a tech-for-good
-          project, not to DemocracyLab itself, and any associated liability is
-          entirely with that project owner.
-        </li>
-        <li>
-          I release and forever discharge and hold harmless DemocracyLab from
-          any and all liability, claims, and demands related to my volunteering
-          for third-party project owners, use of the DemocracyLab website, and
-          attendance at DemocracyLab events.{" "}
-        </li>
-        <li>
-          I understand that if I am responsible for injuries or property damage
-          to project owners or others while acting outside the scope of
-          volunteer assignments, that I may be held personally liable by the
-          injured party.
-        </li>
-        <li>
-          I will perform the project tasks on a temporary, fully-independent
-          basis without significant and ongoing control of work schedules and
-          conditions by the project owner.
-        </li>
-        <li>
-          I am not volunteering for an organization with which I am employed nor
-          am I expecting to receive a job offer from a project owner.
-        </li>
-        <li>
-          I will not perform the type of work an employee would typically
-          perform for a project owner’s customers or clients nor will I
-          supervise employees of a project owner. I will use my own tools and
-          equipment and will not seek reimbursement for any purchases of
-          materials or supplies.
-        </li>
-        <li>
-          I grant DemocracyLab permission to use all photographs, images, video,
-          or audio recordings of me in connection with my attendance at
-          DemocracyLab events or volunteering for project owners.
-        </li>
-        <li>
-          I am at least 18 years old and possess the authority to bind myself to
-          this agreement.
-        </li>
-      </ul>
-    );
+    return <TermsVolunteer />
   }
 
   _renderOrgSignupTerms(): React$Node {
-    return (
-      <React.Fragment>
-        <p>
-          As an organization accepting third-party volunteers via DemocracyLab,
-          I agree:
-        </p>
-        <ul>
-          <li>
-            I am volunteering solely for my personal purposes or pleasure,
-            including for civic, charitable, or humanitarian reasons.
-          </li>
-          <li>
-            I am submitting registration information that is truthful and
-            accurate and will update this information if anything changes.
-          </li>
-          <li>
-            I agree not to use DemocracyLab to violate any law or regulation,
-            violate any third-party right, disrupt DemocracyLab services, or to
-            transmit mass unsolicited advertisements via e-mail or other means.
-          </li>
-          <li>
-            I release, forever discharge, hold harmless, and indemnify
-            DemocracyLab from any and all liability, claims, and demands related
-            to my use of DemocracyLab and use of third-party volunteers via
-            DemocracyLab.
-          </li>
-          <li>
-            I am offering volunteering projects that are civic, charitable, or
-            humanitarian in nature.
-          </li>
-          <li>
-            I accept third-party volunteers that are offering their assistance
-            solely for their own personal civic, charitable, or humanitarian
-            reasons.
-          </li>
-          <li>
-            I have not promised and will not provide compensation or benefits to
-            any third-party volunteer in exchange for their services.
-          </li>
-          <li>
-            I am receiving volunteer services directly from each third-party
-            volunteer and any associated liability is entirely with that
-            volunteer, not with DemocracyLab.
-          </li>
-          <li>
-            I acknowledge that third-party volunteers will perform the project
-            tasks on a temporary, fully-independent basis without significant
-            and ongoing control of work schedules and conditions by my
-            organization.
-          </li>
-          <li>
-            I will not accept via DemocracyLab volunteer services from an
-            employee of my organization.
-          </li>
-          <li>
-            I grant DemocracyLab permission to use all photographs, images,
-            video, or audio recordings of my organization and logo in connection
-            with my use of DemocracyLab and attendance at DemocracyLab events.
-          </li>
-          <li>
-            I acknowledge that DemocracyLab may change these terms and may
-            modify or discontinue the site or services at its sole discretion.
-          </li>
-          <li>
-            I agree that DemocracyLab may terminate my membership or use of the
-            site and services without prior notice and remove and discard my
-            content from the site for any reason, including if DemocracyLab
-            believes I have violated these terms.
-          </li>
-        </ul>
-      </React.Fragment>
-    );
+    return <TermsUse />
   }
 }
 
