228916d421786a3aa956d55c6712b56123574ad0
Peter Breen <pbreen@gmail.com>
2022-07-19 11:22:22 -0700

    add section stubs for tabs

--- common/components/controllers/LandingController.jsx
+++ common/components/controllers/LandingController.jsx
@@ -11,14 +11,10 @@ import cdn from "../utils/cdn";
 import Button from "react-bootstrap/Button";
 import url from "../utils/url";
 import Section from "../enums/Section";
-import Vo1 from "../svg/volunteer1.svg";
-import Vo2 from "../svg/volunteer2.svg";
-import Vo3 from "../svg/volunteer3.svg";
-import ArrowBlack from "../svg/arrowright-black.svg";
-import Rec1 from "../svg/recruit1.svg";
-import Rec2 from "../svg/recruit2.svg";
-import Rec3 from "../svg/recruit3.svg";
-import ArrowOrange from "../svg/arrowright-orange.svg";
+import ProjectChart from "../svg/homepage/chart.svg";
+import GreenSplitDot from "../svg/homepage/green-dot.svg";
+import YellowDot from "../svg/homepage/yellow-dot.svg";
+import RedDot from "../svg/homepage/red-dot.svg";
 
 class LandingController extends React.PureComponent<{||}> {
   constructor(): void {
@@ -58,24 +54,69 @@ class LandingController extends React.PureComponent<{||}> {
     );
   }
 
-  _renderOptions() {
+  _renderOptions(): React$Node {
     return (
       <div className="LandingController-options col-12">
         <h2>I'd like to...</h2>
-        <p>scrollable/clickable row of 3 content boxes go here</p>
+        <p>
+          scrollable/clickable row of 3 content boxes go here: "Volunteer",
+          "Create a Project", and "Be a Partner"
+        </p>
+      </div>
+    );
+  }
+
+  _volunteerSection(): React$Node {
+    return (
+      <div className="LandingController-volunteer-section">
+        <p>Why volunteer with a DemocracyLab Project?</p>
+        <p>3 svgs I don't have yet</p>
+        <Button variant="primary">Volunteer Now</Button>
+      </div>
+    );
+  }
+  \;
+
+  _createProjectSection(): React$Node {
+    return (
+      <React.Fragment>
+        <div className="LandingController-createproject-section">
+          <p>Why create a project on DemocracyLab?</p>
+        </div>
+        <div className="LandingController-chart-section">
+          <p>(chart goes here)</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+
+  _partnerSection(): React$Node {
+    return (
+      <div className="LandingController-partner-section">
+        <p>Why become a sponsor?</p>
+        <Button variant="primary">Become a Sponsor</Button>
+        <p>Why host a tech-for-good-hackathon?</p>
+        <Button variant="primary">Host a Hackathon</Button>
       </div>
     );
   }
 
   _renderNextHackathon(): React$Node {
-    return (<div className="LandingController-next-hackathon col-12">
-    <p>some function that returns a hackathon here i guess, probably don't render section at all if no upcoming?</p>
-    </div>);
+    return (
+      <div className="LandingController-next-hackathon col-12">
+        <p>
+          some function that returns a hackathon here i guess, probably don't
+          render section at all if no upcoming?
+        </p>
+      </div>
+    );
   }
   _renderTestimonials(): React$Node {
     return (
       <div className="LandingController-testimonial-container carousel-testimonial-root col-12">
-        <h2 className="text-center">What people are saying about DemocracyLab</h2>
+        <h2 className="text-center">
+          What people are saying about DemocracyLab
+        </h2>
         <TestimonialCarousel interval={15000} />
       </div>
     );
@@ -86,7 +127,7 @@ class LandingController extends React.PureComponent<{||}> {
       <div className="LandingController-latestblogpost col-12">
         <p>latest blog post here</p>
       </div>
-    )
+    );
   }
 
   _renderCommunityPartners() {
