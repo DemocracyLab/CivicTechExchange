49957d5dae598c7d8fc01b1e887b16a41e345988
Darryl Banks <277740+justBanks@users.noreply.github.com>
2022-07-19 02:18:43 -0700

    latest

--- civictechprojects/urls.py
+++ civictechprojects/urls.py
@@ -84,7 +84,7 @@ urlpatterns = [
     url(r'', include(v1_urls)),
     url(r'^api/team$', views.team, name='team'),
     url(r'^api/project/(?P<project_id>[0-9]+)/$', views.get_project, name='get_project'),
-    url(r'api/project/(?P<project_id>[0-9]+)/volunteers/$',views.get_project_volunteers,name='get_project_volunteers'),
+    url(r'api/project/(?P<project_id>[0-9]+)/volunteers/$', views.get_project_volunteers, name='get_project_volunteers'),
     url(r'^api/group/(?P<group_id>[0-9]+)/invite$', views.invite_project_to_group, name='invite_project_to_group'),
     url(r'^api/invite/(?P<invite_id>[0-9]+)/approve$', views.accept_group_invitation, name='accept_group_invitation'),
     url(r'^api/invite/(?P<invite_id>[0-9]+)/reject$', views.reject_group_invitation, name='reject_group_invitation'),
@@ -105,5 +105,3 @@ urlpatterns = [
     url(r'^api/testimonials/(?P<category>[-\w]*)', views.get_testimonials, name='get_testimonials'),
     url(r'^api/v1/qiqo/webhooks/zoom_presences', views.qiqo_webhook, name='qiqo_webhook'),
 ]
-
-
