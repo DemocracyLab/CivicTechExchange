88e3f7b4414ba0200defc09fea16da0f3d090977
marlonkeating <marlonakeating@gmail.com>
2022-07-28 18:44:42 -0700

    Disallow lists in markdown text

--- common/components/common/richtext/AllowMarkdown.jsx
+++ common/components/common/richtext/AllowMarkdown.jsx
@@ -5,6 +5,8 @@ import ReactMarkdown from "react-markdown";
 
 class AllowMarkdown extends React.PureComponent {
   render(): React$Node {
+    // Disallow headers so as not to interfere with set header hierarchy of page
+    // Disallow list elements because they interact poorly with pre-wrap
     const disallowedElements: ReadonlyArray<string> = [
       "h1",
       "h2",
@@ -12,9 +14,14 @@ class AllowMarkdown extends React.PureComponent {
       "h4",
       "h5",
       "h6",
+      "ol",
+      "li",
     ];
     return (
-      <ReactMarkdown disallowedElements={disallowedElements}>
+      <ReactMarkdown
+        disallowedElements={disallowedElements}
+        unwrapDisallowed={true}
+      >
         {this.props.children}
       </ReactMarkdown>
     );
