fdf5c9c9087204706826b0178cc6ad9b6a38bca6
Peter Breen <pbreen@gmail.com>
2022-09-16 13:30:44 -0700

    use utility class for rotating chart label

--- common/components/controllers/LandingController.jsx
+++ common/components/controllers/LandingController.jsx
@@ -223,7 +223,7 @@ class LandingController extends React.PureComponent<{||}, State> {
         <div className="LandingController-chart-section col-12 col-lg-10 col-xxl-9 ml-lg-auto mr-lg-auto LandingController-section">
           <h2 className="text-center">Tech-for-Good Projects</h2>
           <div className="lc-flex-row">
-            <p class="lc-vertical-left">Organization Type</p>
+            <p class="lc-vertical-left rotate-180">Organization Type</p>
             <div className="lc-gridbox-parent">
               <div className="lc-gridbox">
                 <div className="lc-text">
