d62d0d3d03901e02e23bfe5ce450dd1a7ca1aa33
crazyoptimist <hey@crazyoptimist.net>
2022-12-07 17:05:58 -0600

    chore: enable loading dotenv in manage.py added django-dotenv package

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 
 if __name__ == "__main__":
+    dotenv.read_dotenv()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "democracylab.settings")
     try:
         from django.core.management import execute_from_command_line
