0151419b36117559f12b30c87ee8061ebb1869f7
Workhqd <hqd77813@gmail.com>
2023-03-30 10:35:01 -0700

    .

--- common/helpers/queue.py
+++ common/helpers/queue.py
@@ -34,7 +34,7 @@ from typing import Callable
 from django.conf import settings
 from urllib.parse import urlparse
 
-redis_url = os.getenv('REDIS_URL', 'rediss://:@127.0.0.1:6380/16')
+redis_url = os.getenv('REDIS_URL', 'rediss://localhost:6380/16')
 #redis_url = os.getenv('REDIS_URL','redis://localhost:6379')
 url = urlparse(redis_url)
 # Check if the Redis connection is using SSL/TSL
