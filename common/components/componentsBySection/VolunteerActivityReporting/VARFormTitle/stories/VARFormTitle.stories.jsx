92666cedede2a0947e3f999e31c8dd19f65e06ed
Monica Cruz <mcatcruz@gmail.com>
2025-05-28 15:38:07 -0700

    Updated VARFormTitle Story to render hardcoded h1

--- common/components/componentsBySection/VolunteerActivityReporting/VARFormTitle/stories/VARFormTitle.stories.jsx
+++ common/components/componentsBySection/VolunteerActivityReporting/VARFormTitle/stories/VARFormTitle.stories.jsx
@@ -4,16 +4,9 @@ import VARFormTitle from '../VARFormTitle';
 export default {
     title: 'VolunteerActivityReporting/VARFormTitle',
     component: VARFormTitle,
-    argTypes: {
-        formTitle: {
-            control: 'text',
-        },
-    },
 };
 
-const Template = (args) => <VARFormTitle {...args} />
 
-export const Default = Template.bind({});
-Default.args = {
-    formTitle: 'Weekly Activity Report'
+export const Default = () => {
+   return( <VARFormTitle /> )
 };
